42b8b8c7b611457dd978ca81443f915a6e67c7d2
Binoy Patel <binoypatel14@gmail.com>
2017-02-25 02:16:41 -0500

    Added tests for Button Component
    - Added snapshot test for Text Component
    - Updated tests for Text to use loop to search for style

--- src/text/__tests__/Text.js
+++ src/text/__tests__/Text.js
@@ -5,32 +5,65 @@ import Text from "../Text";
 describe("Text Component", () => {
   it("should render without issues", () => {
     const component = shallow(<Text />);
-    
+
     expect(component.length).toBe(1);
+    expect(component).toMatchSnapshot();
   });
 
   it("should have font size of 50 when h1", () => {
     const component = shallow(<Text h1 />);
 
-    expect(component.props().style[1].fontSize).toBe(50);
+    const styles = component.props().style;
+    let fontSizeStyle;
+    for(let i = 0; i < styles.length; i++) {
+      if(styles[i] && styles[i].hasOwnProperty("fontSize")) {
+        fontSizeStyle = styles[i].fontSize;
+      }
+    }
+
+    expect(fontSizeStyle).toBe(50);
   });
 
   it("should have font size of 42.5 when h2", () => {
     const component = shallow(<Text h2 />);
 
-    expect(component.props().style[2].fontSize).toBe(42.5);
+    const styles = component.props().style;
+    let fontSizeStyle;
+    for(let i = 0; i < styles.length; i++) {
+      if(styles[i] && styles[i].hasOwnProperty("fontSize")) {
+        fontSizeStyle = styles[i].fontSize;
+      }
+    }
+
+    expect(fontSizeStyle).toBe(42.5);
   });
 
   it("should have font size of 35 when h3", () => {
     const component = shallow(<Text h3 />);
 
-    expect(component.props().style[3].fontSize).toBe(35);
+    const styles = component.props().style;
+    let fontSizeStyle;
+    for(let i = 0; i < styles.length; i++) {
+      if(styles[i] && styles[i].hasOwnProperty("fontSize")) {
+        fontSizeStyle = styles[i].fontSize;
+      }
+    }
+
+    expect(fontSizeStyle).toBe(35);
   });
 
   it("should have font size of 27.5 when h4", () => {
     const component = shallow(<Text h4 />);
 
-    expect(component.props().style[4].fontSize).toBe(27.5);
+    const styles = component.props().style;
+    let fontSizeStyle;
+    for(let i = 0; i < styles.length; i++) {
+      if(styles[i] && styles[i].hasOwnProperty("fontSize")) {
+        fontSizeStyle = styles[i].fontSize;
+      }
+    }
+
+    expect(fontSizeStyle).toBe(27.5);
   });
 
   it("should have text as children", () => {
