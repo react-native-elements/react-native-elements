adc32e2299ec0591a487a477a6ee2c7dfa0cfe87
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2017-04-09 15:17:17 -0700

    add tile tests

--- src/tile/__tests__/FeaturedTile.js
+++ src/tile/__tests__/FeaturedTile.js
@@ -9,4 +9,31 @@ describe('FeaturedTitle component', () => {
     expect(component.length).toBe(1);
     expect(component).toMatchSnapshot();
   });
+
+  it('should render with width and height', () => {
+    const component = shallow(<FeaturedTile 
+      imageSrc={{url: 'http://google.com'}}
+      width={34}
+      height={20}
+    />);
+
+    expect(component.length).toBe(1);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should render with Icon', () => {
+    const component = shallow(<FeaturedTile 
+      imageSrc={{url: 'http://google.com'}}
+      icon={{name: 'play-circle', type: 'font-awesome'}}
+      imageContainerStyle={{ height: 70 }}
+      containerStyle={{ height: 70 }}
+      captionStyle={{ color: 'blue' }}
+      iconContainerStyle={{ height: 70 }}
+      titleStyle={{ backgroundColor: 'yellow' }}
+      overlayContainerStyle={{ height: 70 }}
+    />);
+
+    expect(component.length).toBe(1);
+    expect(component).toMatchSnapshot();
+  });
 });
