291113a7e73998fcbc43e5a1da1b5b44e6f079cf
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2017-04-09 04:17:35 -0700

    add icons tests

--- src/icons/__tests__/Icon.js
+++ src/icons/__tests__/Icon.js
@@ -4,9 +4,30 @@ import Icon from '../Icon';
 
 describe('Icon component', () => {
   it('should render without issues', () => {
-    const component = shallow(<Icon name="wifi"/>);
+    const component = shallow(<Icon name='wifi'/>);
 
     expect(component.length).toBe(1);
     expect(component).toMatchSnapshot();
+  });
+
+  it('should render with icon type', () => {
+    const component = shallow(<Icon 
+      name='alert' 
+      type='octicon'
+      reverse
+      color='red'
+      iconStyle={{ backgroundColor: 'peru' }}
+    />);
+
+    expect(component.length).toBe(1);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should have onPress event', () => {
+    const onPress = jest.fn();
+    const component = shallow(<Icon onPress={onPress} name='wifi' />);
+
+    component.simulate('press');
+    expect(onPress).toHaveBeenCalledTimes(1);
   });
 });
