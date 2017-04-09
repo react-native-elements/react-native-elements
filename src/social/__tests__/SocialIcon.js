c02ba6a3f7e303f8732ece24708cf9f0d6b25927
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2017-04-09 15:16:40 -0700

    add social tests

--- src/social/__tests__/SocialIcon.js
+++ src/social/__tests__/SocialIcon.js
@@ -9,4 +9,51 @@ describe('SocialIcon component', () => {
     expect(component.length).toBe(1);
     expect(component).toMatchSnapshot();
   });
+
+  it('should show loading indicator', () => {
+    const component = shallow(<SocialIcon loading />);
+
+    expect(component.find('ActivityIndicator').length).toBe(1);
+  });
+
+  it('should render light social icon', () => {
+    const component = shallow(<SocialIcon
+      light
+      raised={false}
+      type='medium'
+    />);
+
+    expect(component.length).toBe(1);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should render social icon button', () => {
+    const component = shallow(<SocialIcon
+      title='Sign In With Facebook'
+      button
+      type='facebook'
+    />);
+
+    expect(component.length).toBe(1);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should render social icon button', () => {
+    const component = shallow(<SocialIcon
+      title='Sign In With Facebook'
+      button
+      type='facebook'
+    />);
+
+    expect(component.length).toBe(1);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should have onPress event', () => {
+    const onPress = jest.fn();
+    const component = shallow(<SocialIcon onPress={onPress} />);
+
+    component.simulate('press');
+    expect(onPress).toHaveBeenCalled();
+  });
 });
