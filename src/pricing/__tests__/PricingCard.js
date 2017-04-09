5f9613c41165f85abf560d2fdfb8dca2feeef94a
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2017-04-09 04:18:30 -0700

    add pricing tests

--- src/pricing/__tests__/PricingCard.js
+++ src/pricing/__tests__/PricingCard.js
@@ -6,10 +6,28 @@ describe('PricingCard component', () => {
   it('should render without issues', () => {
     const component = shallow(
       <PricingCard 
+        info={['1 User', 'Basic Support', 'All Core Features']}
+        button={{ title: 'GET STARTED', icon: 'flight-takeoff' }}
+      />
+    );
+
+    expect(component.length).toBe(1);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should render with props', () => {
+    const component = shallow(
+      <PricingCard 
         title='Free'
         price='$0'
         info={['1 User', 'Basic Support', 'All Core Features']}
         button={{ title: 'GET STARTED', icon: 'flight-takeoff' }}
+        titleFont='arial'
+        infoFont='arial'
+        pricingFont='arial'
+        buttonFont='arial'
+        containerStyle={{ backgroundColor: 'peru' }}
+        wrapperStyle={{ backgroundColor: 'peru' }}
       />
     );
 
