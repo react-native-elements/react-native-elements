307863d336e07cccb9c2b8c87466c3e4bd5a5e75
Vladislav <kak60g@gmail.com>
2017-04-14 14:24:44 +0300

    Update ButtonGroup.js

--- src/buttons/ButtonGroup.js
+++ src/buttons/ButtonGroup.js
@@ -117,6 +117,7 @@ ButtonGroup.propTypes = {
   onShowUnderlay: PropTypes.func,
   setOpacityTo: PropTypes.any,
   innerBorderStyle: PropTypes.oneOf(View.propTypes.style, NativeText.propTypes.style),
+  lastBorderStyle: PropTypes.oneOf(View.propTypes.style, NativeText.propTypes.style),
   buttonStyle: View.propTypes.style,
   selectedBackgroundColor: PropTypes.string,
 };
