2f965050cf6bc234c46e2d4a5a6ea5162cd13d01
Binoy Patel <binoypatel14@gmail.com>
2017-04-28 19:28:53 -0400

    Updated PropTypes to reference prop-types package

--- src/form/FormValidationMessage.js
+++ src/form/FormValidationMessage.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 import { StyleSheet, View, Text as NativeText } from 'react-native';
 import colors from '../config/colors';
 import Text from '../text/Text';
--- src/grid/Row.js
+++ src/grid/Row.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 import { View, StyleSheet, TouchableOpacity } from 'react-native';
 
 const Row = props => {
