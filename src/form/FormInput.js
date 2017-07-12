9d3890ea61aab2b220c119c7d777ff3269752cce
Guizmo Martinez <martinezguillaume@users.noreply.github.com>
2017-07-12 15:55:46 +0200

    Add `shake` props to `FormInput` component

--- src/form/FormInput.js
+++ src/form/FormInput.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import {
+  Animated,
+  Easing,
   TextInput,
   StyleSheet,
   View,
@@ -15,6 +17,13 @@ import ViewPropTypes from '../config/ViewPropTypes';
 const { width } = Dimensions.get('window');
 
 class FormInput extends Component {
+  componentWillMount() {
+    this.shakeAnimationValue = new Animated.Value(0);
+    this.props.shake && this.shake();
+  }
+  componentWillReceiveProps(nextProps) {
+    nextProps.shake && this.props.shake !== nextProps.shake && this.shake();
+  }
   focus() {
     const ref = this.props.textInputRef;
     this.refs[ref].focus();
@@ -22,6 +31,15 @@ class FormInput extends Component {
   blur() {
     const ref = this.props.textInputRef;
     this.refs[ref].blur();
+  }
+  shake() {
+    const { shakeAnimationValue } = this;
+    shakeAnimationValue.setValue(0);
+    Animated.timing(shakeAnimationValue, {
+      duration: 300,
+      toValue: 3,
+      ease: Easing.bounce,
+    }).start();
   }
   render() {
     const {
@@ -31,17 +49,27 @@ class FormInput extends Component {
       containerRef,
       ...attributes
     } = this.props;
+    const translateX = this.shakeAnimationValue.interpolate({
+      inputRange: [0, 0.5, 1, 1.5, 2, 2.5, 3],
+      outputRange: [0, -15, 0, 15, 0, -15, 0],
+    });
     return (
-      <View
+      <Animated.View
         ref={containerRef}
-        style={[styles.container, containerStyle && containerStyle]}
+        style={[
+          styles.container,
+          containerStyle && containerStyle,
+          {
+            transform: [{ translateX }],
+          },
+        ]}
       >
         <TextInput
           ref={textInputRef}
           style={[styles.input, inputStyle && inputStyle]}
           {...attributes}
         />
-      </View>
+      </Animated.View>
     );
   }
 }
