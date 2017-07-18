49282c71eb17721340e6bc39bad6616304a11c10
Monte9 <Monte9@users.noreply.github.com>
2017-07-17 23:33:24 -0700

    added trinagle login views

--- example/src/views/input_home.js
+++ example/src/views/input_home.js
@@ -2,7 +2,6 @@ import Expo from 'expo';
 import React, { Component } from 'react';
 import { View, ScrollView, StyleSheet, Text, Dimensions} from 'react-native';
 
-import Icon from 'react-native-vector-icons/FontAwesome';
 import SimpleIcon from 'react-native-vector-icons/SimpleLineIcons';
 import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
@@ -11,88 +10,64 @@ import Input from '../../beta/input/Input';
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class InputHome extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      email: '',
-      password: ''
-    };
-  }
-
   render() {
-    const { email, password } = this.state;
-
     return (
       <ScrollView style={styles.container}>
         <View style={styles.contentView}>
-          <Text style={{color: 'rgba(114,208,193,1)', fontWeight: '500', fontSize: 22}}>Login</Text>
-          <View style={{
-              justifyContent: 'center', alignItems: 'center', marginTop: 10, width: 300
-            }}
-          >
-            <Input
-              icon={
-                <Icon
-                  name='user-o'
-                  color='rgba(171, 189, 219, 1)'
-                  size={25}
-                />
-              }
-              containerStyle={{width: 300}}
-              onChangeText={email => this.setState({email})}
-              value={email}
-              inputStyle={{marginLeft: 10, color: 'black'}}
-              keyboardAppearance="light"
-              placeholder="name@example.com"
-              autoFocus={false}
-              autoCapitalize="none"
-              autoCorrect={false}
-              keyboardType="email-address"
-              returnKeyType="next"
-              ref={ input => this.emailInput = input }
-              onSubmitEditing={() => {
-                this.passwordInput.focus();
-              }}
-              blurOnSubmit={false}
-              placeholderTextColor="rgba(171, 189, 219, 1)"
-              displayError={true}
-              errorStyle={{textAlign: 'center'}}
-              errorMessage="Please enter a valid email address"
-            />
-          </View>
-          <View style={{
-              justifyContent: 'center', alignItems: 'center', width: 300
-            }}
-          >
-            <Input
-              icon={
-                <Icon
+          <View style={{backgroundColor: '#2F343B', width: SCREEN_WIDTH, alignItems: 'center'}}>
+            <Text style={{color: 'white', fontSize: 30, marginVertical: 10, fontWeight: '300', marginTop: 10}}>Login</Text>
+            <View style={styles.overlay}>
+              <View style={styles.triangleLeft} />
+              <Input
+                containerStyle={{borderWidth: 1, borderColor: 'white', borderLeftWidth: 0, height: 50, width: SCREEN_WIDTH - 80, backgroundColor: 'white'}}
+                icon={
+                  <MaterialIcon
+                    name='email-outline'
+                    color='black'
+                    size={25}
+                  />
+                }
+                placeholder="Email"
+                placeholderTextColor="black"
+                autoCapitalize="none"
+                autoCorrect={false}
+                keyboardAppearance="light"
+                keyboardType="email-address"
+                returnKeyType="next"
+                ref={ input => this.emailInput = input }
+                onSubmitEditing={() => {
+                  this.passwordInput.focus();
+                }}
+                blurOnSubmit={false}
+              />
+              <View style={styles.triangleRight} />
+            </View>
+            <View style={[styles.overlay, { marginBottom: 30, marginTop: 1 }]}>
+              <View style={styles.triangleLeft} />
+              <Input
+                containerStyle={{borderWidth: 1, borderColor: 'white', borderLeftWidth: 0, height: 50, width: SCREEN_WIDTH - 80, backgroundColor: 'white'}}
+                icon={
+                  <SimpleIcon
                   name='lock'
-                  color='rgba(171, 189, 219, 1)'
+                  color='black'
                   size={25}
                 />
-              }
-              containerStyle={{width: 300}}
-              onChangeText={(password) => this.setState({password})}
-              value={password}
-              inputStyle={{marginLeft: 10, color: 'black'}}
-              secureTextEntry={true}
-              keyboardAppearance="light"
-              placeholder="password"
-              autoCapitalize="none"
-              autoCorrect={false}
-              keyboardType="default"
-              returnKeyType="done"
-              ref={ input => this.passwordInput = input}
-              blurOnSubmit={true}
-              placeholderTextColor="rgba(171, 189, 219, 1)"
-              displayError={true}
-              errorStyle={{textAlign: 'center'}}
-              errorMessage="The email and password you entered did not match out records. Please try again!"
-            />
+                }
+                placeholder="Password"
+                placeholderTextColor="black"
+                autoCapitalize="none"
+                keyboardAppearance="light"
+                secureTextEntry={true}
+                autoCorrect={false}
+                keyboardType="default"
+                returnKeyType="done"
+                ref={ input => this.passwordInput = input }
+                blurOnSubmit={true}
+              />
+              <View style={styles.triangleRight} />
+            </View>
           </View>
-          <View style={{backgroundColor: 'rgba(46, 50, 72, 1)', width: SCREEN_WIDTH, alignItems: 'center', marginTop: 20}}>
+          <View style={{backgroundColor: 'rgba(46, 50, 72, 1)', width: SCREEN_WIDTH, alignItems: 'center'}}>
             <Text style={{color: 'white', fontSize: 30, marginVertical: 10, fontWeight: '300'}}>Sign up</Text>
             <Input
               containerStyle={{borderRadius: 40, borderWidth: 1, borderColor: 'rgba(110, 120, 170, 1)', height: 50, width: SCREEN_WIDTH - 50, marginVertical: 10}}
@@ -202,10 +177,35 @@ const styles = StyleSheet.create({
   },
   contentView: {
     flex: 1,
-    marginTop: 10,
     justifyContent: 'center',
     alignItems: 'center'
-  }
+  },
+  triangleLeft: {
+    position: 'absolute',
+    left: -20,
+    bottom: 0,
+    width: 0,
+    height: 0,
+    borderRightWidth: 20,
+    borderRightColor: 'white',
+    borderBottomWidth: 25,
+    borderBottomColor: 'transparent',
+    borderTopWidth: 25,
+    borderTopColor: 'transparent'
+  },
+  triangleRight: {
+    position: 'absolute',
+    right: -20,
+    top: 0,
+    width: 0,
+    height: 0,
+    borderLeftWidth: 20,
+    borderLeftColor: 'white',
+    borderBottomWidth: 25,
+    borderBottomColor: 'transparent',
+    borderTopWidth: 25,
+    borderTopColor: 'transparent'
+  },
 });
 
 export default InputHome;
