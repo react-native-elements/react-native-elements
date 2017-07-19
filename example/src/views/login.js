04a2f723cea1a99521e0c476d268b253bad92760
Monte9 <Monte9@users.noreply.github.com>
2017-07-18 17:17:43 -0700

    added login screens; good template to add login screens

--- example/src/views/login.js
+++ example/src/views/login.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
-import { ScrollView, View, Text } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
 
-class Login extends Component {
+import LoginScreen1 from './login/screen1';
+import LoginScreen2 from './login/screen2';
+import LoginScreen3 from './login/screen3';
+import LoginScreen4 from './login/screen4';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+export default class Login extends Component {
   render() {
     return (
-      <ScrollView style={{ backgroundColor: 'white' }}>
-        <Text>Hello world!</Text>
-      </ScrollView>
+      <View style={styles.container}>
+        <ScrollView
+          ref={(snapScroll) => { this.snapScroll = snapScroll; }}
+          horizontal={true}
+          decelerationRate={0}
+          onResponderRelease={()=>{
+            var interval = SCREEN_WIDTH; // WIDTH OF 1 CHILD COMPONENT
+
+            var snapTo = (this.scrollingRight)? Math.ceil(this.lastx / interval) :
+              Math.floor(this.lastx / interval);
+            var scrollTo = snapTo * interval;
+            this.snapScroll.scrollTo({x: scrollTo, y: 0, animated: true});
+          }}
+          scrollEventThrottle={32}
+          onScroll={(event)=>{
+            var nextx = event.nativeEvent.contentOffset.x;
+            this.scrollingRight = (nextx > this.lastx);
+            this.lastx = nextx;
+          }}
+          showsHorizontalScrollIndicator={false}
+          style={styles.listViewHorizontal}
+        >
+          <LoginScreen1 />
+          <LoginScreen2 />
+          <LoginScreen3 />
+          <LoginScreen4 />
+        </ScrollView>
+      </View>
     );
   }
 }
 
-export default Login;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
