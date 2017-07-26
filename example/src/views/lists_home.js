5f589d76e2de02804aaabebbef8b44bc0fbfbdef
Xavier Villelégier <x.villelegier@gmail.com>
2017-07-26 20:29:49 +0200

    Fix deprecated Grid Row Col

--- example/src/views/lists_home.js
+++ example/src/views/lists_home.js
@@ -14,9 +14,6 @@ import {
   Card,
   ButtonGroup,
   Tile,
-  Grid,
-  Col,
-  Row,
   Icon,
   List,
   ListItem,
@@ -229,37 +226,6 @@ class Icons extends Component {
           </Card>
           <Card
             containerStyle={{ marginTop: 15, marginBottom: 15 }}
-            title="GRID LAYOUT"
-          >
-            <Grid
-              containerStyle={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-                alignSelf: 'center',
-                padding: 10,
-              }}
-            >
-              <Col size={0.25}>
-                <TouchableHighlight onPress={() => console.log('hello')}>
-                  <Image
-                    source={require('../images/avatar1.jpg')}
-                    style={{ height: 50, width: 50, borderRadius: 25 }}
-                  />
-                </TouchableHighlight>
-              </Col>
-              <Col>
-                <Row size={0.75}>
-                  <Text style={{ fontSize: 22 }}>Adam McArther</Text>
-                </Row>
-                <Row>
-                  <Text style={{ color: 'grey' }}>Freelance developer</Text>
-                </Row>
-              </Col>
-            </Grid>
-          </Card>
-          <Card
-            containerStyle={{ marginTop: 15, marginBottom: 15 }}
             title="TILES"
           >
             <View>
@@ -327,8 +293,8 @@ class Icons extends Component {
             }}
             title="AVATARS"
           >
-            <Grid containerStyle={{margin: 40}}>
-              <Row containerStyle={{justifyContent: 'center', alignItems: 'center'}}>
+            <View style={{margin: 40, flex: 1}}>
+              <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
                 <Avatar
                   small
                   rounded
@@ -364,8 +330,8 @@ class Icons extends Component {
                   containerStyle={{margin: 10}}
                   activeOpacity={0.7}
                 />
-              </Row>
-              <Row containerStyle={{justifyContent: 'center', alignItems: 'center', marginTop: 80}}>
+              </View>
+              <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: 80}}>
                 <Avatar
                   medium
                   rounded
@@ -387,8 +353,8 @@ class Icons extends Component {
                   containerStyle={{margin: 10}}
                   activeOpacity={0.7}
                 />
-              </Row>
-            </Grid>
+              </View>
+            </View>
           </Card>
         </View>
       </ScrollView>
