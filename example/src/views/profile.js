97c839250af810cb82b80e4ece9b6c8a2262e5b1
Monte9 <Monte9@users.noreply.github.com>
2017-08-06 11:56:24 -0700

    added placeholder profile screens

--- example/src/views/profile.js
+++ example/src/views/profile.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { StyleSheet, View, ScrollView } from 'react-native';
 
 import ProfileScreen1 from './profile/screen1';
+import ProfileScreen2 from './profile/screen2';
+import ProfileScreen3 from './profile/screen3';
+import ProfileScreen4 from './profile/screen4';
 
 export default class Login extends Component {
   render() {
@@ -13,8 +16,9 @@ export default class Login extends Component {
           decelerationRate={0.993}
         >
           <ProfileScreen1 />
-          <ProfileScreen1 />
-          <ProfileScreen1 />
+          <ProfileScreen2 />
+          <ProfileScreen3 />
+          <ProfileScreen4 />
         </ScrollView>
       </View>
     );
