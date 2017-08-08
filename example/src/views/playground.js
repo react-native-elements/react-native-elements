568302d227a0a51ab89c11f97a1c0277cac4d22f
Monte9 <Monte9@users.noreply.github.com>
2017-08-08 01:52:59 -0700

    added demo for callbacks for tagGroups

--- example/src/views/playground.js
+++ example/src/views/playground.js
@@ -13,7 +13,7 @@ class CustomCheckbox extends Component {
   }
 
   render() {
-    const { tag } = this.props;
+    const { tag, color, updateCount } = this.props;
     const { checked } = this.state;
 
     return (
@@ -27,19 +27,60 @@ class CustomCheckbox extends Component {
         uncheckedIcon='add'
         checkedColor='red'
         checked={checked}
-        onPress={() => this.setState({checked: !checked})}
+        containerStyle={{ width: 70, backgroundColor: color}}
+        onPress={() => {
+          this.setState({
+            checked: !checked
+          });
+          updateCount(!this.state.checked);
+        }}
       />
     );
   }
 }
 
-class Playground extends Component {
-  renderTags() {
-    return ['One', 'Two', 'Three', 'Four'].map((tag, index) => {
-      return <CustomCheckbox key={index} tag={tag} />;
+class CustomTagGroups extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      count: 0
+    };
+  }
+
+  renderTags(tags, color) {
+    return tags.map((tag, index) => {
+      return (
+        <View key={index} style={{ flex: 1, flexDirection: 'column', width: 70 }}>
+          <CustomCheckbox tag={tag} color={color} updateCount={this.updateCount.bind(this)} />
+        </View>
+      );
     });
   }
 
+  updateCount(increment) {
+    const { count } = this.state;
+
+    this.setState({
+      count: increment ? count + 1 : count - 1
+    });
+  }
+
+  render() {
+    const { tags, color } = this.props;
+    const { count } = this.state;
+
+    return (
+      <View style={{ flex: 1 }}>
+        <Text style={{ textAlign: 'center' }}>Group 1</Text>
+        {this.renderTags(tags, color)}
+        <Text style={{ textAlign: 'center' }}>Count: {count}</Text>
+      </View>
+    );
+  }
+}
+
+class Playground extends Component {
   render() {
     return (
       <ScrollView style={{ backgroundColor: 'white' }}>
@@ -58,7 +99,12 @@ class Playground extends Component {
             title='VIEW NOW'
           />
         </Card>
-        {this.renderTags()}
+        <View style={{flexDirection: 'row', marginVertical: 30}}>
+          <CustomTagGroups tags={['5', '3', '2', '9']} color='green' />
+          <CustomTagGroups tags={['10', '5', '7', '3']} color='blue' />
+          <CustomTagGroups tags={['1', '2', '3', '4']} color='pink' />
+          <CustomTagGroups tags={['18', '9', '0', '2']} color='orange' />
+        </View>
       </ScrollView>
     );
   }
