7c3ad3bda2f7e6e3f2910c6f69dc7eb0e970c02d
Monte9 <Monte9@users.noreply.github.com>
2017-08-14 20:11:16 -0700

    wrap up avatar view

--- example/src/views/lists/screen1.js
+++ example/src/views/lists/screen1.js
@@ -1,11 +1,11 @@
-import _ from 'lodash'
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Dimensions, ScrollView } from 'react-native';
 
 import { Font } from 'expo';
 
 import Avatar from '../../../v1/avatar/Avatar';
-import Button from '../../../v1/buttons/Button'
+import Button from '../../../v1/buttons/Button';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 
@@ -70,7 +70,7 @@ export default class ListsScreen1 extends Component {
   }
 
   renderValue(user) {
-    const { value, positive } = user
+    const { value, positive } = user;
 
     if (positive) {
       return (
@@ -82,7 +82,7 @@ export default class ListsScreen1 extends Component {
           />
           <Text style={{color: 'green', fontFamily: 'regular', fontSize: 13, marginLeft: 5}}>{value}</Text>
         </View>
-      )
+      );
     } else {
       return (
         <View style={{ backgroundColor: 'rgba(244,230,224,1)', width: 70, height: 28, borderRadius: 5, justifyContent: 'center', alignItems: 'center', flexDirection: 'row', marginLeft: 10}}>
@@ -93,12 +93,12 @@ export default class ListsScreen1 extends Component {
           />
           <Text style={{color: 'red', fontFamily: 'regular', fontSize: 13, marginLeft: 5}}>{value}</Text>
         </View>
-      )
+      );
     }
   }
 
   renderCard(user, index) {
-    const { name, avatar } = user
+    const { name, avatar } = user;
 
     return (
       <View key={index} style={{height: 60, marginHorizontal: 20, marginTop: 10, backgroundColor: 'white', borderRadius: 5, alignItems: 'center', flexDirection: 'row'}}>
@@ -128,13 +128,13 @@ export default class ListsScreen1 extends Component {
           </View>
         </View>
       </View>
-    )
+    );
   }
 
   renderListCards() {
     return _.map(USERS, (user, index) => {
-      return this.renderCard(user, index)
-    })
+      return this.renderCard(user, index);
+    });
   }
 
   render() {
@@ -149,7 +149,7 @@ export default class ListsScreen1 extends Component {
               </Text>
             </View>
             <ScrollView style={{flex: 1, marginBottom: 20}}>
-              <View style={{flex: 1, flexDirection: 'column', backgroundColor: 'white', borderRadius: 5, alignItems: 'center', height: 250, marginHorizontal: 20, marginBottom: 10, marginTop: 20}}>
+              <View style={{flex: 1, flexDirection: 'column', backgroundColor: 'white', borderRadius: 5, alignItems: 'center', height: 250, marginHorizontal: 20, marginBottom: 10}}>
                 <View style={{flex: 3, flexDirection: 'row'}}>
                   <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
                     <Avatar
@@ -163,10 +163,12 @@ export default class ListsScreen1 extends Component {
                       overlayContainerStyle={{backgroundColor: 'transparent'}}
                     />
                   </View>
-                  <View style={{flex: 1, backgroundColor: 'yellow'}}>
-                    <Text style={{fontFamily: 'regular', fontSize: 15, marginLeft: 10, color: 'gray'}}>
-                      Johh Smith
-                    </Text>
+                  <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                    <View style={{ flex: 1, marginTop: 10, justifyContent: 'center'}}>
+                      <Text style={{ fontFamily: 'bold', fontSize: 25, color: 'rgba(98,93,144,1)', marginLeft: -15}}>
+                        Paul Allen
+                      </Text>
+                    </View>
                   </View>
                 </View>
                 <View style={{width: 300, borderWidth: 0.5, borderColor: 'rgba(222, 223, 226, 1)', marginHorizontal: 20, height: 1, marginVertical: 10}} />
