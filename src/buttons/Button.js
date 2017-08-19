dab996b50d9cbaf1a93a9f7b2f9d799801786f59
Xavier Villelégier <x.villelegier@gmail.com>
2017-08-19 17:17:55 +0200

    Fix ripple effect on inner views

--- src/buttons/Button.js
+++ src/buttons/Button.js
@@ -137,6 +137,7 @@ const Button = props => {
         {...attributes}
       >
         <View
+          pointerEvents="box-only"
           style={[
             styles.button,
             secondary && { backgroundColor: colors.secondary },
