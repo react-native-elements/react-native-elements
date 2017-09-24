728db6919986b492ed9628758e312585e1f0807a
Xavier Villelégier <x.villelegier@gmail.com>
2017-09-24 13:45:16 +0200

    Update SearchBar for iOS

--- example/v1/searchbar/SearchBar.js
+++ example/v1/searchbar/SearchBar.js
@@ -1,20 +1,41 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import Ionicon from 'react-native-vector-icons/Ionicons';
 import ViewPropTypes from '../config/ViewPropTypes';
 import Input from '../input/Input';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class SearchBar extends Component {
 
   render() {
     const {
-      ...attributes
+      inputStyle,
+      containerStyle,
+      iconStyle,
+      placeholderTextColor,
+      iconContainerStyle,
+      noIcon,
+      icon,
+      ...attributes,
     } = this.props;
     return (
+        <View style={styles.container}>
         <Input
-          {...attributes}
-        />
+            inputStyle={[styles.input, inputStyle]}
+            containerStyle={[styles.inputContainer, containerStyle]}
+            icon={noIcon ? undefined : (icon ? icon : <Ionicon
+              size={20}
+              name={'ios-search'}
+              color={(iconStyle && iconStyle.color )|| '#7d7d7d'}
+              />)}
+            iconStyle={iconStyle}
+            iconContainerStyle={[styles.iconContainerStyle, iconContainerStyle]}
+            placeholderTextColor={placeholderTextColor || '#7d7d7d'}
+            {...attributes}
+          />
+        </View>
     );
   }
 }
@@ -23,6 +44,29 @@ SearchBar.propTypes = {};
 
 SearchBar.defaultProps = {};
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f5f5f5',
+    paddingBottom: 13,
+    paddingTop: 13,
+    borderBottomWidth: 1,
+    borderBottomColor: '#c4c4c4',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    marginLeft: 5,
+  },
+  inputContainer: {
+    backgroundColor: '#dcdce1',
+    borderRadius: 9,
+    height: 36,
+    width: SCREEN_WIDTH - 30,
+    borderBottomWidth: 0,
+  },
+  iconContainerStyle: {
+    marginLeft: 8,
+  }
+});
 
 export default SearchBar;
