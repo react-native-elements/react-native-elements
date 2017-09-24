556dbd9352968c074a18a168bd30df993dc5da51
Xavier Villelégier <x.villelegier@gmail.com>
2017-09-24 15:26:22 +0200

    Add `cancelButton` props

--- example/v1/searchbar/SearchBar.js
+++ example/v1/searchbar/SearchBar.js
@@ -1,6 +1,12 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { StyleSheet, View, Dimensions } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Dimensions,
+  Button,
+  LayoutAnimation
+} from 'react-native';
 import Ionicon from 'react-native-vector-icons/Ionicons';
 import ViewPropTypes from '../config/ViewPropTypes';
 import Input from '../input/Input';
@@ -8,6 +14,37 @@ import Input from '../input/Input';
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class SearchBar extends Component {
+
+  focus() {
+    this.input.focus();
+  }
+
+  blur() {
+    this.input.blur();
+  }
+
+  clear() {
+    this.input.clear();
+  }
+
+  onFocus = () => {
+    this.props.onFocus && this.props.onFocus();
+    LayoutAnimation.easeInEaseOut();
+    this.setState({ hasFocus: true });
+  }
+
+  onBlur = () => {
+    this.props.onBlur && this.props.onBlur();
+    LayoutAnimation.easeInEaseOut();
+    this.setState({ hasFocus: false });
+  }
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasFocus: false,
+    };
+  }
 
   render() {
     const {
@@ -18,13 +55,27 @@ class SearchBar extends Component {
       iconContainerStyle,
       noIcon,
       icon,
+      clearButtonMode,
+      cancelButton,
+      onFocus,
+      onBlur,
       ...attributes,
     } = this.props;
+    const { hasFocus } = this.state;
+    
     return (
         <View style={styles.container}>
-        <Input
+          <Input
+            onFocus={this.onFocus}
+            onBlur={this.onBlur}
+            clearButtonMode={clearButtonMode || 'while-editing'}
+            ref={input => (this.input = input)}
             inputStyle={[styles.input, inputStyle]}
-            containerStyle={[styles.inputContainer, containerStyle]}
+            containerStyle={[
+              styles.inputContainer,
+              (!hasFocus || !cancelButton) && { width: SCREEN_WIDTH - 30, marginRight: 15 },
+              containerStyle,
+            ]}
             icon={noIcon ? undefined : (icon ? icon : <Ionicon
               size={20}
               name={'ios-search'}
@@ -35,6 +86,10 @@ class SearchBar extends Component {
             placeholderTextColor={placeholderTextColor || '#7d7d7d'}
             {...attributes}
           />
+          {cancelButton && <Button
+            title={'Cancel'}
+            onPress={() => this.input.blur()}
+          />}
         </View>
     );
   }
@@ -51,18 +106,19 @@ const styles = StyleSheet.create({
     paddingTop: 13,
     borderBottomWidth: 1,
     borderBottomColor: '#c4c4c4',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: 'row',
+    width: SCREEN_WIDTH,
   },
   input: {
     marginLeft: 5,
+    flex: 1,
   },
   inputContainer: {
     backgroundColor: '#dcdce1',
     borderRadius: 9,
     height: 36,
-    width: SCREEN_WIDTH - 30,
     borderBottomWidth: 0,
+    marginLeft: 15,
   },
   iconContainerStyle: {
     marginLeft: 8,
