16228f89690ed311bafd8d65916de7f247b3d078
Jeffrey Shek <jeffshek@gmail.com>
2017-11-23 18:13:48 -0500

    Fix typo in home.js

--- example/src/drawer/home.js
+++ example/src/drawer/home.js
@@ -88,7 +88,7 @@ Home.navigationOptions = {
         alignItems: 'center',
         justifyContent: 'center',
       }}
-      type="material-commnity"
+      type="material-community"
       color={tintColor}
     />
   ),
