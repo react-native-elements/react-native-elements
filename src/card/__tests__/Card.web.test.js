f7760268a5468e2f485b39b9362eb27105eba458
Gregor71 <millergregor@gmail.com>
2018-01-22 22:24:14 +0000

    tests: update Card snapshots

--- src/card/__tests__/Card.web.test.js
+++ src/card/__tests__/Card.web.test.js
@@ -15,70 +15,6 @@ import options, {
 } from '../../../samples';
 import Card from '../Card';
 
-describe('Card Component', () => {
-  it('should render without issues', () => {
-    const component = shallow(<Card />);
-
-    expect(component.length).toBe(1);
-    expect(toJson(component)).toMatchSnapshot();
-  });
-
-  it('should have Card title without image', () => {
-    const component = shallow(
-      <Card
-        title="Card Title"
-        containerStyle={{ backgroundColor: 'red' }}
-        fontFamily="arial"
-        dividerStyle={{ backgroundColor: 'red' }}
-        flexDirection="row"
-      />
-    );
-
-    expect(component.length).toBe(1);
-    expect(toJson(component)).toMatchSnapshot();
-  });
-
-  it('should have Card title with image', () => {
-    const component = shallow(
-      <Card
-        title="HELLO WORLD"
-        image={{
-          uri:
-            'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-        }}
-        containerStyle={{ backgroundColor: 'red' }}
-        titleStyle={{ backgroundColor: 'red' }}
-        fontFamily="arial"
-      />
-    );
-
-    expect(component.length).toBe(1);
-    expect(toJson(component)).toMatchSnapshot();
-  });
-
-  it('should have Card with featured title', () => {
-    const component = shallow(
-      <Card
-        title="foo title"
-        image={{
-          uri:
-            'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-        }}
-        imageWrapperStyle={{ backgroundColor: 'red' }}
-        imageStyle={{ backgroundColor: 'red' }}
-        wrapperStyle={{ backgroundColor: 'red' }}
-        featuredTitle="featured title"
-        featuredSubtitle="featured sub title"
-        featuredTitleStyle={{ backgroundColor: 'red' }}
-        featuredSubtitleStyle={{ backgroundColor: 'red' }}
-      />
-    );
-
-    expect(component.length).toBe(1);
-    expect(toJson(component)).toMatchSnapshot();
-  });
-});
-
 const children = [
   <View key="v0" style={{ width: 50, height: 50, backgroundColor: '#071' }} />,
   <View key="v1" style={{ width: 50, height: 50, backgroundColor: '#293' }} />,
@@ -263,3 +199,67 @@ parseSamples(
   },
   options
 );
+
+describe('Card Component', () => {
+  it('should render without issues', () => {
+    const component = shallow(<Card />);
+
+    expect(component.length).toBe(1);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('should have Card title without image', () => {
+    const component = shallow(
+      <Card
+        title="Card Title"
+        containerStyle={{ backgroundColor: 'red' }}
+        fontFamily="arial"
+        dividerStyle={{ backgroundColor: 'red' }}
+        flexDirection="row"
+      />
+    );
+
+    expect(component.length).toBe(1);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('should have Card title with image', () => {
+    const component = shallow(
+      <Card
+        title="HELLO WORLD"
+        image={{
+          uri:
+            'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+        }}
+        containerStyle={{ backgroundColor: 'red' }}
+        titleStyle={{ backgroundColor: 'red' }}
+        fontFamily="arial"
+      />
+    );
+
+    expect(component.length).toBe(1);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('should have Card with featured title', () => {
+    const component = shallow(
+      <Card
+        title="foo title"
+        image={{
+          uri:
+            'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+        }}
+        imageWrapperStyle={{ backgroundColor: 'red' }}
+        imageStyle={{ backgroundColor: 'red' }}
+        wrapperStyle={{ backgroundColor: 'red' }}
+        featuredTitle="featured title"
+        featuredSubtitle="featured sub title"
+        featuredTitleStyle={{ backgroundColor: 'red' }}
+        featuredSubtitleStyle={{ backgroundColor: 'red' }}
+      />
+    );
+
+    expect(component.length).toBe(1);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+});
