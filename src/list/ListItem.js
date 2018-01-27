452c921490498a56bf6b29c92708dd6481707d58
Keith Lim <Keith@Keiths-MacBook-Pro.local>
2018-01-27 00:45:04 -0500

    added listItem right icon size so that it takes rightIcon size, or 28

--- src/list/ListItem.js
+++ src/list/ListItem.js
@@ -245,7 +245,7 @@ const ListItem = props => {
               <Icon
                 type={rightIcon.type}
                 iconStyle={rightIcon.style}
-                size={28}
+                size={rightIcon.size || 28}
                 name={rightIcon.name || 'chevron-right'}
                 color={rightIcon.color || chevronColor}
               />
