4549c21f57f4d6060582fbcda69221873ca12201
Kamil Rykowski <kamil.rykowski@stxnext.pl>
2018-03-11 10:42:32 +0100

    Revert previous git hook #942

--- src/avatar/Avatar.js
+++ src/avatar/Avatar.js
@@ -123,7 +123,9 @@ const Avatar = props => {
       );
     } else if (title) {
       return (
-        <Text style={[styles.title, titleStyle && titleStyle]}>{title}</Text>
+        <Text style={[styles.title, titleStyle && titleStyle]}>
+          {title}
+        </Text>
       );
     } else if (icon) {
       return (
