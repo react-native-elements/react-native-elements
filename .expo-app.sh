c901cdf517cff3565d6e25d95ffc15260e9f8571
Xavier Villelégier <x.villelegier@gmail.com>
2018-03-18 17:02:01 +0100

    Update script

--- .expo-app.sh
+++ .expo-app.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    COMMENT="This is a text"
-curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-  -d "{\"body\": \"$COMMENT\"}" \
-  "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+  # Configuring user
+  git config --global user.email "x.villelegier@gmail.com"
+  git config --global user.name "Xavier Villelégier"
+  echo "machine github.com login xavier-villelegier password $GITHUB_TOKEN" > ~/.netrc
+
+  # Cloning ex
+  git clone git@github.com:react-native-training/react-native-elements-app.git
+  cd react-native-elements-app
+  yarn add "https://github.com/${TRAVIS_REPO_SLUG}.git\#${TRAVIS_COMMIT}"
+  yarn
+  exp login -u "$EXPO_LOGIN" -p "$EXPO_PASSWORD"
+  COMMENT= $(exp publish --release-channel ${TRAVIS_COMMIT})
+  curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+    -d "{\"body\": \"$COMMENT\"}" \
+    "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
