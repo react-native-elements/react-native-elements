5a125aa7d135308b9fadec4c7547c27f2cff43a5
Xavier Villelégier <x.villelegier@gmail.com>
2018-03-18 15:42:25 +0100

    Move out curl command

---
+++ .expo-app.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+COMMENT="This is a text"
+curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+  -d "{\"body\": \"$COMMENT\"}" \
+  "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
