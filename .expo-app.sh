585d4e22312c66a6cc419ad3e94903abb903deab
Xavier Villelégier <x.villelegier@gmail.com>
2018-03-18 16:02:50 +0100

    Move condition

--- .expo-app.sh
+++ .expo-app.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-COMMENT="This is a text"
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    COMMENT="This is a text"
 curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
   -d "{\"body\": \"$COMMENT\"}" \
   "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+fi
