433ccda26ebaafd78fcdad71a50c0fb59e0603a4
Xavier Villelégier <x.villelegier@gmail.com>
2018-03-18 18:46:09 +0100

    Update script

--- .expo-app.sh
+++ .expo-app.sh
@@ -7,17 +7,24 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   git config --global user.name "Xavier Villelégier"
   echo -e "machine github.com\n  login xavier-villelegier\n  password $GITHUB_TOKEN" >> ~/.netrc
 
-  # Cloning examples app
+  # Cloning examples app and installing modules
   git clone "https://github.com/react-native-training/react-native-elements-app.git"
   cd react-native-elements-app
-  yarn add "https://github.com/${TRAVIS_REPO_SLUG}.git#${TRAVIS_COMMIT}"
+  yarn add "https://github.com/${TRAVIS_REPO_SLUG}.git#${TRAVIS_PULL_REQUEST_SHA}"
   yarn
   yarn global add exp
+  yarn global add qrcode-terminal
+
+  # Login into expo and publishing app
   set +x
   exp login -u "$EXPO_LOGIN" -p "$EXPO_PASSWORD"
   set -x
-  COMMENT=$(exp publish --release-channel ${TRAVIS_COMMIT})
+  exp publish --release-channel ${TRAVIS_PULL_REQUEST_SHA}
+
+  # Comment the PR
+  EXPO_URL="https://exp.host/@xavier-villelegier/react-native-elements-app?release-channel=${TRAVIS_PULL_REQUEST_SHA}"
+  COMMENT="URL: $EXPO_URL\n\n$(qrcode-terminal $EXPO_URL)"
   curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-    -d "{\"body\": \"$(echo $COMMENT)\"}" \
+    -d "{\"body\": \"$(echo -e $COMMENT)\"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
