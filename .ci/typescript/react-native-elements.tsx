4a438cfc810216944df6f442aedea5fe99d758c6
Kyle Roach <kroach.work@gmail.com>
2018-04-10 09:49:34 -0400

    Add PricingCard test

--- .ci/typescript/react-native-elements.tsx
+++ .ci/typescript/react-native-elements.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { StyleProp, TouchableHighlight, ViewStyle } from 'react-native';
-import { Badge, Divider, Text } from '../../src/index';
+import { Badge, Divider, PricingCard, Text } from '../../src/index';
 
 const testViewStyle: StyleProp<ViewStyle> = {
   backgroundColor: 'pink',
@@ -26,6 +26,22 @@ const BadgeTest = () => (
 );
 
 const DividerTest = () => <Divider style={testViewStyle} />;
+
+const PricingCardTest = () => (
+  <PricingCard
+    color="#4f9deb"
+    title="Free"
+    price="$0"
+    info={['1 User', 'Basic Support', 'All Core Features']}
+    button={{ title: 'GET STARTED', icon: 'flight-takeoff' }}
+    buttonFont="somefont"
+    infoFont="somefont"
+    titleFont="somefont"
+    containerStyle={testViewStyle}
+    wrapperStyle={testViewStyle}
+    onButtonPress={() => null}
+  />
+);
 
 const TextTest = () => (
   <Text h1 h2 h3 h4 fontFamily="papyrus" style={testViewStyle}>
