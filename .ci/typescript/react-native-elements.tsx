f2bccffbe57754a6648d8ddac199b1396fb4a423
Kyle Roach <kroach.work@gmail.com>
2018-04-10 09:03:41 -0400

    Add Text test

--- .ci/typescript/react-native-elements.tsx
+++ .ci/typescript/react-native-elements.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
-import { Button, Divider } from '../../src/index';
+import { StyleProp, ViewStyle } from 'react-native';
+import { Button, Divider, Text } from '../../src/index';
 
-class AvatarTest extends React.Component<{}> {
-  render() {
-    return <Button />;
-  }
-}
+const testViewStyle: StyleProp<ViewStyle> = {
+  backgroundColor: 'pink',
+};
 
-class DividerTest extends React.Component<{}> {
-  render() {
-    return <Divider style={{ backgroundColor: 'pink' }} />;
-  }
-}
+const AvatarTest = () => <Button />;
+
+const DividerTest = () => <Divider style={testViewStyle} />;
+
+const TextTest = () => (
+  <Text h1 h2 h3 h4 fontFamily="papyrus" style={testViewStyle}>
+    Hey!
+  </Text>
+);
