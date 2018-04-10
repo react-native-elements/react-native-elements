e42df6e917316f6d617a62c7edff8a987787c847
Kyle Roach <kroach.work@gmail.com>
2018-04-10 08:44:08 -0400

    Add Divider test

--- .ci/typescript/react-native-elements.tsx
+++ .ci/typescript/react-native-elements.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
-import { Button } from '../../src/index';
+import { Button, Divider } from '../../src/index';
 
 class AvatarTest extends React.Component<{}> {
   render() {
     return <Button />;
+  }
+}
+
+class DividerTest extends React.Component<{}> {
+  render() {
+    return <Divider style={{ backgroundColor: 'pink' }} />;
   }
 }
