4aeb63dec7a9147454c068474b453779cf06f9ca
Guizmo <martinezguillaum@gmail.com>
2018-04-27 15:21:55 +0200

    Add renderNode

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import {
+  Platform,
+  StatusBar,
+  StyleSheet,
+  View,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+} from 'react-native';
+import renderNode from '../helpers/renderNode';
 import Text from '../text/Text';
 import Icon from '../icons/Icon';
+import Button from '../buttons/Button';
+import Avatar from '../avatar/Avatar';
 import { colors, ViewPropTypes, getStatusBarHeight } from '../config';
 
 const ALIGN_STYLE = {
@@ -11,40 +21,48 @@ const ALIGN_STYLE = {
   center: 'center',
 };
 
-const Children = ({ style, placement, children }) => (
-  <View style={[style, placement && { alignItems: ALIGN_STYLE[placement] }]}>
-    {children == null ? null : React.isValidElement(children) ? (
-      children
-    ) : children.title ? (
-      <Text {...children}>{children.title}</Text>
-    ) : children.icon ? (
-      <Icon
-        {...children}
-        containerStyle={[
-          { alignItems: ALIGN_STYLE[placement] },
-          children.containerStyle,
-        ]}
-        name={children.icon}
-      />
-    ) : null}
-  </View>
-);
+// Prevent breaking changes
+const renderComponent = content =>
+  content == null || content === false ? null : React.isValidElement(
+    content
+  ) ? (
+    content
+  ) : content.text || typeof content === 'string' ? (
+    <Text {...content}>{content.text}</Text>
+  ) : content.icon ? (
+    <Icon {...content} name={content.icon} />
+  ) : null;
 
 const Header = ({
+  onPress,
+  onLongPress,
+  Component = onPress || onLongPress ? TouchableOpacity : View,
   statusBarProps,
-  left,
-  center,
-  right,
+  leftComponent,
+  centerComponent,
+  rightComponent,
   leftContainerStyle,
   centerContainerStyle,
   rightContainerStyle,
   backgroundColor,
   containerStyle,
   placement,
+  leftButton,
+  centerButton,
+  rightButton,
+  leftIcon,
+  centerIcon,
+  rightIcon,
+  leftAvatar,
+  centerAvatar,
+  rightAvatar,
   ...attributes
 }) => (
-  <View
+  <Component
+    activeOpacity={1}
     {...attributes}
+    onPress={onPress}
+    onLongPress={onLongPress}
     style={[
       styles.container,
       backgroundColor && { backgroundColor },
@@ -52,38 +70,47 @@ const Header = ({
     ]}
   >
     <StatusBar barStyle="light-content" {...statusBarProps} />
-    <Children
-      style={[styles.rightLeftContainer, leftContainerStyle]}
-      placement="left"
+    <View style={[styles.leftContainer, leftContainerStyle]}>
+      {renderComponent(leftComponent)}
+      {renderNode(Button, leftButton)}
+      {renderNode(Icon, leftIcon)}
+      {renderNode(Avatar, leftAvatar)}
+    </View>
+    <View
+      style={[
+        styles.centerContainer,
+        placement && { alignItems: ALIGN_STYLE[placement] },
+        centerContainerStyle,
+      ]}
     >
-      {left}
-    </Children>
-    <Children
-      style={[styles.centerContainer, centerContainerStyle]}
-      placement={placement}
-    >
-      {center}
-    </Children>
-    <Children
-      style={[styles.rightLeftContainer, rightContainerStyle]}
-      placement="right"
-    >
-      {right}
-    </Children>
-  </View>
+      {renderComponent(centerComponent)}
+      {renderNode(Button, centerButton)}
+      {renderNode(Icon, centerIcon)}
+      {renderNode(Avatar, centerAvatar)}
+    </View>
+    <View style={[styles.rightContainer, rightContainerStyle]}>
+      {renderComponent(rightComponent)}
+      {renderNode(Button, rightButton)}
+      {renderNode(Icon, rightIcon)}
+      {renderNode(Avatar, rightAvatar)}
+    </View>
+  </Component>
 );
+
+
 
 Header.propTypes = {
   placement: PropTypes.oneOf(['left', 'center', 'right']),
-  left: PropTypes.object,
-  center: PropTypes.object,
-  right: PropTypes.object,
+  leftComponent: PropTypes.object,
+  centerComponent: PropTypes.object,
+  rightComponent: PropTypes.object,
   leftContainerStyle: ViewPropTypes.style,
   centerContainerStyle: ViewPropTypes.style,
   rightContainerStyle: ViewPropTypes.style,
   backgroundColor: PropTypes.string,
   containerStyle: ViewPropTypes.style,
   statusBarProps: PropTypes.object,
+  leftButton: ,
 };
 
 Header.defaultProps = {
@@ -95,19 +122,24 @@ const styles = StyleSheet.create({
     backgroundColor: colors.primary,
     borderBottomColor: '#f2f2f2',
     borderBottomWidth: StyleSheet.hairlineWidth,
-    paddingHorizontal: 10,
     paddingTop: getStatusBarHeight(),
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     height: (Platform.OS === 'ios' ? 44 : 56) + getStatusBarHeight(),
+    paddingHorizontal: 8,
+  },
+  leftContainer: {
+    flex: 1,
+    alignItems: 'flex-start',
   },
   centerContainer: {
-    flex: 3,
-    paddingHorizontal: Platform.OS === 'ios' ? 15 : 16,
+    flex: 2,
+    alignItems: 'center',
   },
-  rightLeftContainer: {
+  rightContainer: {
     flex: 1,
+    alignItems: 'flex-end',
   },
 });
 
