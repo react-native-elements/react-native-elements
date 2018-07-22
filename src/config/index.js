09c7dbd3f809cfa2ac94634d07f6600847d7b48f
Kyle Roach <kroach.work@gmail.com>
2018-07-22 17:09:11 -0400

    Add ThemeProvider

--- src/config/index.js
+++ src/config/index.js
@@ -3,5 +3,14 @@ import colors from './colors';
 import { getStatusBarHeight } from './statusBar';
 import ViewPropTypes from './ViewPropTypes';
 import fonts from './fonts';
+import ThemeProvider, { ThemeConsumer } from './ThemeProvider';
 
-export { BackgroudImage, colors, getStatusBarHeight, ViewPropTypes, fonts };
+export {
+  BackgroudImage,
+  colors,
+  getStatusBarHeight,
+  ViewPropTypes,
+  fonts,
+  ThemeProvider,
+  ThemeConsumer,
+};
