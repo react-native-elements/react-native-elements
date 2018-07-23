7981d8da3e82ee889b4d0d7269eae6a9d2621d89
Kyle Roach <kroach.work@gmail.com>
2018-07-23 04:35:31 -0400

    Merge incoming theme

--- src/config/ThemeProvider.js
+++ src/config/ThemeProvider.js
@@ -1,34 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import merge from 'lodash.merge';
 
 const ThemeContext = React.createContext();
 
 export default class ThemeProvider extends React.Component {
-  state = {
-    theme: {
-      colors: {
-        primary: '#2089dc',
-        secondary: '#8F0CE8',
-        grey0: '#393e42',
-        grey1: '#43484d',
-        grey2: '#5e6977',
-        grey3: '#86939e',
-        grey4: '#bdc6cf',
-        grey5: '#e1e8ee',
-        greyOutline: '#bbb',
-        searchBg: '#303337',
-        error: '#ff190c',
-      }
-    },
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      theme: {
+        ...merge(
+          {
+            colors: {
+              primary: '#2089dc',
+              secondary: '#8F0CE8',
+              grey0: '#393e42',
+              grey1: '#43484d',
+              grey2: '#5e6977',
+              grey3: '#86939e',
+              grey4: '#bdc6cf',
+              grey5: '#e1e8ee',
+              greyOutline: '#bbb',
+              searchBg: '#303337',
+              error: '#ff190c',
+            },
+          },
+          props.theme
+        ),
+      },
+    };
+  }
 
   updateTheme = updates => {
     this.setState(({ theme }) => ({
-      theme: {
-        ...theme,
-        ...updates,
-      },
+      theme: merge(theme, updates),
     }));
+  };
+
+  getTheme = () => {
+    return this.state.theme;
   };
 
   render() {
@@ -47,7 +58,7 @@ export default class ThemeProvider extends React.Component {
 
 ThemeProvider.propTypes = {
   theme: PropTypes.object,
-  children: PropTypes.node,
+  children: PropTypes.node.isRequired,
 };
 
 export const ThemeConsumer = ThemeContext.Consumer;
