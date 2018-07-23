093979cb2988d424e70110819796e8a2f28a3f25
Kyle Roach <kroach.work@gmail.com>
2018-07-23 09:07:25 -0400

    Add theming to Button

--- src/config/ThemeProvider.js
+++ src/config/ThemeProvider.js
@@ -9,26 +9,24 @@ export default class ThemeProvider extends React.Component {
     super(props);
 
     this.state = {
-      theme: {
-        ...merge(
-          {
-            colors: {
-              primary: '#2089dc',
-              secondary: '#8F0CE8',
-              grey0: '#393e42',
-              grey1: '#43484d',
-              grey2: '#5e6977',
-              grey3: '#86939e',
-              grey4: '#bdc6cf',
-              grey5: '#e1e8ee',
-              greyOutline: '#bbb',
-              searchBg: '#303337',
-              error: '#ff190c',
-            },
+      theme: merge(
+        {
+          colors: {
+            primary: '#2089dc',
+            secondary: '#8F0CE8',
+            grey0: '#393e42',
+            grey1: '#43484d',
+            grey2: '#5e6977',
+            grey3: '#86939e',
+            grey4: '#bdc6cf',
+            grey5: '#e1e8ee',
+            greyOutline: '#bbb',
+            searchBg: '#303337',
+            error: '#ff190c',
           },
-          props.theme
-        ),
-      },
+        },
+        props.theme
+      ),
     };
   }
 
