97a53cf1d7766149b0f1f3bc8934e572fd0c3de8
Kyle Roach <kroach.work@gmail.com>
2018-10-09 18:18:06 -0400

    feat(theme): Pass updateTheme through the hoc

--- src/config/withTheme.js
+++ src/config/withTheme.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { merge, ThemeConsumer } from './index';
 
-// This function takes a component...
 const withTheme = (WrappedComponent, themeKey) => {
-  // ...and returns another component...
   class ThemedComponent extends React.PureComponent {
     render() {
       return (
         <ThemeConsumer>
-          {({ theme }) => (
+          {({ theme, updateTheme }) => (
             <WrappedComponent
               {...merge({}, themeKey && theme[themeKey], this.props)}
               theme={theme}
+              updateTheme={updateTheme}
             />
           )}
         </ThemeConsumer>
