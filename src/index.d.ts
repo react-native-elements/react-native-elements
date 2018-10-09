5a65c9b2fcedd1a7d90e41da71433d732a4d8232
Alex Hinson <alexmhinson@gmail.com>
2018-10-09 12:00:31 -0400

    fix(docs, type): Change `clearText` references to `clear`  (#1448)
    Changes `clearText` to `clear` for searchbar

--- src/index.d.ts
+++ src/index.d.ts
@@ -1498,7 +1498,7 @@ export class SearchBar extends React.Component<SearchBarProps, any> {
   /**
    * Call clear on the TextInput
    */
-  clearText(): void;
+  clear(): void;
 }
 
 export interface SliderProps {
