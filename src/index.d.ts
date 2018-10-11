ea502132e724c8af182ce03c764c5e77778276b5
Jan Hesters <jan.hesters@rwth-aachen.de>
2018-10-11 19:27:13 +0200

    Add TextStyle to possible checkmark types.

--- src/index.d.ts
+++ src/index.d.ts
@@ -1022,7 +1022,7 @@ export interface ListItemProps {
   contentContainerStyle?: StyleProp<ViewStyle>;
   rightContentContainerStyle?: StyleProp<ViewStyle>;
   chevron?: boolean | IconProps;
-  checkmark?: boolean | IconProps;
+  checkmark?: boolean | IconProps | TextStyle;
   onPress?(): void;
   onLongPress?(): void;
   title?: string | React.ReactElement<{}>;
