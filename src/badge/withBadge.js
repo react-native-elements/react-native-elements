1a0542bb5ac432b096dba838390260923d72996c
Jan Hesters <31096420+janhesters@users.noreply.github.com>
2018-11-30 16:58:21 +0100

    Add options parameter to withBadge.js

--- src/badge/withBadge.js
+++ src/badge/withBadge.js
@@ -1,54 +1,57 @@
 import React from "react";
 import { Platform, StyleSheet, View } from "react-native";
-import { Badge } from "react-native-elements";
+import Badge from "./Badge";
 
 const styles = StyleSheet.create({
   badge: {
     borderRadius: 9,
     height: 18,
+    minWidth: 0,
+    width: 18
+  },
+  badgeContainer: {
+    position: "absolute",
     shadowColor: "black",
     shadowOffset: {
       width: 0,
       height: 0.5
     },
     shadowRadius: 0.5,
-    shadowOpacity: 1.0,
-    padding: 0,
-    width: 18
-  },
-  badgeContainer: {
-    position: "absolute"
+    shadowOpacity: 1.0
   },
   badgeText: {
-    fontSize: 10
+    fontSize: 10,
+    paddingHorizontal: 0
   }
 });
 
-const withBadge = (
-  value,
-  status = "error",
-  offSetX = 10,
-  offSetY = 22,
-  badgeProps = {},
-  hidden = typeof value === "number"
-    ? value === 0
-    : typeof value === "string"
-    ? value !== ""
-    : value !== undefined && value !== null;
-) => WrappedComponent =>
+const withBadge = (value, options = {}) => WrappedComponent =>
   class extends React.Component {
     render() {
+      const {
+        status = "error",
+        left = 10,
+        bottom = 22,
+        top = 0,
+        right = 0,
+        hidden = typeof value === "number"
+          ? value === 0
+          : typeof value === "string"
+          ? value !== ""
+          : value !== undefined && value !== null,
+        badgeProps = {}
+      } = options;
       const badgeValue = typeof value === "function" ? value(this.props) : value;
       return (
         <View>
           <WrappedComponent {...this.props} />
           {!hidden && (
             <Badge
-              status={status}
               badgeStyle={styles.badge}
               textStyle={styles.badgeText}
               value={badgeValue}
-              containerStyle={[styles.badgeContainer, { bottom: offSetY, left: offSetX }]}
+              status={status}
+              containerStyle={[styles.badgeContainer, { bottom, left, right, top }]}
               {...badgeProps}
             />
           )}
