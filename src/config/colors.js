22d6d1f5ac21961ebf7e3ba1ef51c43f306b332c
Jan Hesters <31096420+janhesters@users.noreply.github.com>
2018-12-03 15:52:16 +0100

    Add custom keys for the respective platform colors

--- src/config/colors.js
+++ src/config/colors.js
@@ -1,10 +1,7 @@
 import { Platform, StyleSheet } from 'react-native';
 
 export default {
-  primary: Platform.select({
-    default: '#2196f3', // rgb(33, 150, 243) Android
-    ios: '#007aff' // rgb(0, 122, 255)
-  }),
+  primary: '#2089dc',
   secondary: '#8F0CE8',
   grey0: '#393e42',
   grey1: '#43484d',
@@ -14,19 +11,24 @@ export default {
   grey5: '#e1e8ee',
   greyOutline: '#bbb',
   searchBg: '#303337',
-  success: Platform.select({
-    default: '#4caf50', // Android
-    ios: '#4cd964'
-  }),
-  error: Platform.select({
-    default: '#f44336', // Android
-    ios: '#ff3b30'
-  }),
-  warning: Platform.select({
-    default: '#ffeb3b', // Android
-    ios: '#ffcc00'
-  }),
+  success: '#52c41a',
+  error: '#ff190c',
+  warning: '#faad14',
   disabled: 'hsl(208, 8%, 90%)',
   // Darker color if hairlineWidth is not thin enough
   divider: StyleSheet.hairlineWidth < 1 ? '#bcbbc1' : 'rgba(0, 0, 0, 0.12)',
+  ios: {
+    primary: '#007aff',
+    secondary: '#5856d6',
+    success: '#4cd964',
+    error: '#ff3b30',
+    warning: '#ffcc00'
+  },
+  android: {
+    primary: '#2196f3',
+    secondary: '#9C27B0',
+    success: '#4caf50',
+    error: '#f44336',
+    warning: '#ffeb3b'
+  }
 };
