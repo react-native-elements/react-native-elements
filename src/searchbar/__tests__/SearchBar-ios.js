57afd4a72f5b956d19009ef7132088732f4129e8
MHase <maciek.sykula@gmail.com>
2018-12-10 13:27:51 +0100

    Add snapshot test for showCancel prop

--- src/searchbar/__tests__/SearchBar-ios.js
+++ src/searchbar/__tests__/SearchBar-ios.js
@@ -1,7 +1,21 @@
+import React from 'react';
+import { shallow } from 'enzyme';
 import SearchBar from '../SearchBar-ios';
 import { commonTests, commonPlatformTest } from './common';
+import toJson from 'enzyme-to-json';
+
+import theme from '../../config/theme';
 
 describe('iOS SearchBar component', () => {
   commonTests(SearchBar);
   commonPlatformTest(SearchBar);
+
+  describe('Props', () => {
+    it('showCancel', () => {
+      const component = shallow(<SearchBar theme={theme} showCancel />);
+
+      expect(component.length).toBe(1);
+      expect(toJson(component)).toMatchSnapshot();
+    });
+  });
 });
