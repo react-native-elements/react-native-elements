815933d9651ef1685e35eb34c9d8f5598887ade4
Kyle Roach <kroach.work@gmail.com>
2019-02-10 23:35:56 -0400

    feat(Text): Add style props to customize header props
    Allows user to set the styling that should be applied when props
    h1, h2, h3, h4 are used.
    
    Implements #1754

--- src/text/Text.js
+++ src/text/Text.js
@@ -6,17 +6,29 @@ import { fonts, withTheme } from '../config';
 import normalize from '../helpers/normalizeText';
 
 const TextElement = props => {
-  const { style, children, h1, h2, h3, h4, ...rest } = props;
+  const {
+    style,
+    children,
+    h1,
+    h2,
+    h3,
+    h4,
+    h1Style,
+    h2Style,
+    h3Style,
+    h4Style,
+    ...rest
+  } = props;
 
   return (
     <Text
       style={StyleSheet.flatten([
         styles.text,
         style && style,
-        h1 && { fontSize: normalize(40) },
-        h2 && { fontSize: normalize(34) },
-        h3 && { fontSize: normalize(28) },
-        h4 && { fontSize: normalize(22) },
+        h1 && StyleSheet.flatten([{ fontSize: normalize(40) }, h1Style]),
+        h2 && StyleSheet.flatten([{ fontSize: normalize(34) }, h2Style]),
+        h3 && StyleSheet.flatten([{ fontSize: normalize(28) }, h3Style]),
+        h4 && StyleSheet.flatten([{ fontSize: normalize(22) }, h4Style]),
         h1 && styles.bold,
         h2 && styles.bold,
         h3 && styles.bold,
@@ -35,6 +47,10 @@ TextElement.propTypes = {
   h2: PropTypes.bool,
   h3: PropTypes.bool,
   h4: PropTypes.bool,
+  h1Style: Text.propTypes.style,
+  h2Style: Text.propTypes.style,
+  h3Style: Text.propTypes.style,
+  h4Style: Text.propTypes.style,
   children: PropTypes.node,
 };
 
@@ -44,6 +60,11 @@ TextElement.defaultProps = {
   h3: false,
   h4: false,
   style: {},
+  h1Style: {},
+  h2Style: {},
+  h3Style: {},
+  h4Style: {},
+  children: '',
 };
 
 const styles = StyleSheet.create({
