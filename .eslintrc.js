2dbbffdff0519fd499828b3647242623461f652c
Rubie <alex.rubner@gmail.com>
2019-03-25 14:29:30 +0000

    reverting change to eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
   extends: ['airbnb', 'plugin:jest/recommended', 'prettier', 'prettier/react'],
   env: {
     es6: true,
-    es7: true,
     node: true,
     browser: true,
   },
