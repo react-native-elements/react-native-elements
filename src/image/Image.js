6ff04d6328ffcdd28b510bde3f3fc5af5b1ce10a
Kyle Roach <kroach.work@gmail.com>
2019-08-01 21:39:20 -0400

    refactor: Base Image on ImageBackground
    Same layout now exist for iOS and Android allowing requests like #1953 possible.

--- src/image/Image.js
+++ src/image/Image.js
@@ -1,110 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import {
   Animated,
   Image as RNImage,
-  Platform,
   StyleSheet,
   View,
+  ImageBackground,
+  Platform,
 } from 'react-native';
 
 import { nodeType } from '../helpers';
 import { ViewPropTypes, withTheme } from '../config';
 
-class Image extends React.PureComponent {
-  placeholderContainerOpacity = new Animated.Value(1);
+const Image = ({
+  placeholderStyle,
+  PlaceholderContent,
+  containerStyle,
+  style,
+  ImageComponent,
+  ...attributes
+}) => {
+  const [placeholderOpacity] = useState(new Animated.Value(1));
 
-  onLoadEnd = () => {
-    /* Images finish loading in the same frame for some reason,
-        the images will fade in separately with staggerNonce */
+  const onLoad = () => {
     const minimumWait = 100;
     const staggerNonce = 200 * Math.random();
 
     setTimeout(
-      () =>
-        Animated.timing(this.placeholderContainerOpacity, {
+      () => {
+        Animated.timing(placeholderOpacity, {
           toValue: 0,
           duration: 350,
-          useNativeDriver: true,
-        }).start(),
-      minimumWait + staggerNonce
+          useNativeDriver: Platform.OS === 'android' ? false : true,
+        }).start();
+      },
+      Platform.OS === 'android' ? 0 : Math.floor(minimumWait + staggerNonce)
     );
   };
 
-  render() {
-    const {
-      placeholderStyle,
-      PlaceholderContent,
-      containerStyle,
-      style,
-      ImageComponent,
-      ...attributes
-    } = this.props;
+  return (
+    <View style={StyleSheet.flatten([styles.container, containerStyle])}>
+      <ImageComponent
+        {...attributes}
+        onLoad={onLoad}
+        style={style}
+        testID="RNE__Image"
+      />
 
-    return (
-      <View style={StyleSheet.flatten([styles.container, containerStyle])}>
-        {Platform.select({
-          android: (
-            <React.Fragment>
-              <View style={styles.placeholderContainer}>
-                <Animated.View
-                  testID="RNE__Image__placeholder"
-                  style={StyleSheet.flatten([
-                    style,
-                    styles.placeholder,
-                    {
-                      backgroundColor: this.placeholderContainerOpacity.interpolate(
-                        {
-                          inputRange: [0, 1],
-                          outputRange: [
-                            styles.placeholder.backgroundColor,
-                            'transparent',
-                          ],
-                        }
-                      ),
-                    },
-                    placeholderStyle,
-                  ])}
-                >
-                  {PlaceholderContent}
-                </Animated.View>
-              </View>
-
-              <ImageComponent {...attributes} style={style} />
-            </React.Fragment>
-          ),
-          default: (
-            <React.Fragment>
-              <ImageComponent
-                {...attributes}
-                onLoadEnd={this.onLoadEnd}
-                style={style}
-              />
-
-              <Animated.View
-                style={StyleSheet.flatten([
-                  styles.placeholderContainer,
-                  { opacity: this.placeholderContainerOpacity },
-                ])}
-              >
-                <View
-                  testID="RNE__Image__placeholder"
-                  style={StyleSheet.flatten([
-                    style,
-                    styles.placeholder,
-                    placeholderStyle,
-                  ])}
-                >
-                  {PlaceholderContent}
-                </View>
-              </Animated.View>
-            </React.Fragment>
-          ),
-        })}
-      </View>
-    );
-  }
-}
+      <Animated.View
+        style={[
+          styles.placeholderContainer,
+          {
+            opacity: placeholderOpacity,
+          },
+        ]}
+      >
+        <View
+          testID="RNE__Image__placeholder"
+          style={StyleSheet.flatten([
+            style,
+            styles.placeholder,
+            placeholderStyle,
+          ])}
+        >
+          {PlaceholderContent}
+        </View>
+      </Animated.View>
+    </View>
+  );
+};
 
 const styles = {
   container: {
@@ -130,7 +94,8 @@ Image.propTypes = {
 };
 
 Image.defaultProps = {
-  ImageComponent: RNImage,
+  ImageComponent: ImageBackground,
+  style: {},
 };
 
 export { Image };
