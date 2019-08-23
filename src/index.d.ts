e26e7315184d8669e673f281cfa47d87659e61af
Paul Mathis <paul.mathis@gmail.com>
2019-08-23 07:43:33 -0500

    fix(types): Allow passing component for Input label (#2008)

--- src/index.d.ts
+++ src/index.d.ts
@@ -873,7 +873,7 @@ export interface InputProps extends TextInputProperties {
   /**
    * 	Adds label (optional)
    */
-  label?: string;
+  label?: string | React.ReactElement<{}>;
 
   /**
    *  props to be passed to the React Native Text component used to display the label (optional)
