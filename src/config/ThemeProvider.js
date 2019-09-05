react-native-elements/react-native-elements

commit 49ca4d0f2d306d01d8e42d90bd57e2978d016d3c

feat: Export ThemeContext for usage with hooks (#2049)
Resolves #2007

src/config/ThemeProvider.js
@@ -4,7 +4,9 @@ import deepmerge from 'deepmerge';
 
 import colors from './colors';
 
-const ThemeContext = React.createContext();
+export const ThemeContext = React.createContext({
+  colors,
+});
 
 export default class ThemeProvider extends React.Component {
   constructor(props) {

src/config/index.js
@@ -5,7 +5,7 @@ import BackgroundImage from './BackgroundImage';
 import colors from './colors';
 import ViewPropTypes from './ViewPropTypes';
 import fonts from './fonts';
-import ThemeProvider, { ThemeConsumer } from './ThemeProvider';
+import ThemeProvider, { ThemeConsumer, ThemeContext } from './ThemeProvider';
 import withTheme from './withTheme';
 
 const TextPropTypes = Text.propTypes;
@@ -19,5 +19,6 @@ export {
   fonts,
   ThemeProvider,
   ThemeConsumer,
+  ThemeContext,
   withTheme,
 };

src/index.d.ts
@@ -2063,6 +2063,8 @@ export interface ThemeConsumerProps<T> {
 
 export class ThemeConsumer<T> extends React.Component<ThemeConsumerProps<T>> {}
 
+export const ThemeContext: React.Context<ThemeProps<{}>>;
+
 export function withTheme<P = {}, T = {}>(
   component: React.ComponentType<P & ThemeProps<T>>
 ): React.ComponentClass<P>;

src/index.js
@@ -36,7 +36,13 @@ import {
 
 // helpers
 import Text from './text/Text';
-import { colors, ThemeProvider, ThemeConsumer, withTheme } from './config';
+import {
+  colors,
+  ThemeProvider,
+  ThemeConsumer,
+  ThemeContext,
+  withTheme,
+} from './config';
 import getIconType, { registerCustomIconType } from './helpers/getIconType';
 import normalize from './helpers/normalizeText';
 
@@ -71,6 +77,7 @@ export {
   Overlay,
   ThemeProvider,
   ThemeConsumer,
+  ThemeContext,
   withBadge,
   withTheme,
   Image,

commit eb73e7060070335f72c90e906660739c8e2b3f11

fix: Regression with initial context value
 Bug introduced in https://github.com/react-native-training/react-native-elements/commit/49ca4d0f2d306d01d8e42d90bd57e2978d016d3c

src/config/ThemeProvider.js
@@ -5,7 +5,9 @@ import deepmerge from 'deepmerge';
 import colors from './colors';
 
 export const ThemeContext = React.createContext({
-  colors,
+  theme: {
+    colors,
+  },
 });
 
 export default class ThemeProvider extends React.Component {