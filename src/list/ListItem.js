67c9e89ecbc1153fccea717afea0b87ddbba7ddd
Kyle Roach <kroach.work@gmail.com>
2019-10-03 16:11:25 -0400

    fix(ListItem): Update PadView to work on web

--- src/list/ListItem.js
+++ src/list/ListItem.js
@@ -348,20 +348,32 @@ ListItem.defaultProps = {
   title: '',
 };
 
-const PadView = ({ children, pad, Component, ...props }) => {
-  const childrens = React.Children.toArray(children);
-  const { length } = childrens;
-  const Container = Component || View;
-  return (
-    <Container {...props}>
-      {React.Children.map(
-        childrens,
-        (child, index) =>
-          child && [child, index !== length - 1 && <View width={pad} />]
-      )}
-    </Container>
-  );
-};
+class PadView extends React.Component {
+  constructor(props) {
+    super(props);
+    this._root = React.createRef();
+  }
+
+  setNativeProps = nativeProps => {
+    this._root.current.setNativeProps(nativeProps);
+  };
+
+  render() {
+    const { children, pad, Component, ...props } = this.props;
+    const childrens = React.Children.toArray(children);
+    const { length } = childrens;
+    const Container = Component || View;
+    return (
+      <Container {...props} ref={this._root}>
+        {React.Children.map(
+          childrens,
+          (child, index) =>
+            child && [child, index !== length - 1 && <View width={pad} />]
+        )}
+      </Container>
+    );
+  }
+}
 
 PadView.propTypes = {
   children: PropTypes.node,
