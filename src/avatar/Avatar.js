328a63014f8107475069d3d871de41582db312e2
Xavier Villelégier <x.villelegier@gmail.com>
2020-03-04 20:03:07 +0100

    Fix Avatar flickering using deep comparison

--- src/avatar/Avatar.js
+++ src/avatar/Avatar.js
@@ -11,6 +11,7 @@ import {
   TouchableNativeFeedback,
   TouchableWithoutFeedback,
 } from 'react-native';
+import isEqual from 'lodash.isequal'
 
 import { withTheme, ViewPropTypes } from '../config';
 import { renderNode, nodeType } from '../helpers';
@@ -32,7 +33,7 @@ const defaultEditButton = {
   underlayColor: '#000',
 };
 
-const Avatar = ({
+const AvatarComponent = ({
   onPress,
   onLongPress,
   Component = onPress || onLongPress ? TouchableOpacity : View,
@@ -189,7 +190,7 @@ const styles = StyleSheet.create({
   },
 });
 
-Avatar.propTypes = {
+AvatarComponent.propTypes = {
   Component: PropTypes.oneOf([
     View,
     TouchableOpacity,
@@ -229,7 +230,7 @@ Avatar.propTypes = {
   ImageComponent: PropTypes.elementType,
 };
 
-Avatar.defaultProps = {
+AvatarComponent.defaultProps = {
   showEditButton: false,
   onEditPress: null,
   size: 'small',
@@ -237,5 +238,6 @@ Avatar.defaultProps = {
   ImageComponent: RNImage,
 };
 
+const Avatar = React.memo(AvatarComponent, isEqual)
 export { Avatar };
 export default withTheme(Avatar, 'Avatar');
