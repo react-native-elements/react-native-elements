7303ca0a4e59ca9277608d64ad52a0d08f668abd
flyingcircle <jeremyelijah@live.com>
2020-04-27 20:54:36 -0700

    Haruelrovix test/increase coverage for socialicon (#2334)
    * test: Increase code coverage for SocialIcon component
    
    * fix additional SocialIcon Tests
    
    Co-authored-by: haruelrovix <surat@havit.web.id>
    Co-authored-by: Jeremy Hamilton <jeremy.hamilton@tekfive.com>

--- src/social/__tests__/SocialIcon.js
+++ src/social/__tests__/SocialIcon.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ActivityIndicator } from 'react-native';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import { create } from 'react-test-renderer';
@@ -21,9 +22,51 @@ describe('SocialIcon component', () => {
     expect(component.find('ActivityIndicator').length).toBe(1);
   });
 
+  it('should show loading indicator in small size', () => {
+    const component = create(<SocialIcon loading small="small" />);
+
+    const activityIndicator = component.root.findByType(ActivityIndicator);
+    expect(activityIndicator.props.size).toEqual('small');
+  });
+
+  it('should show loading indicator in white color', () => {
+    const component = create(<SocialIcon loading iconColor={null} />);
+
+    const activityIndicator = component.root.findByType(ActivityIndicator);
+    expect(activityIndicator.props.color).toEqual('white');
+  });
+
   it('should render light social icon', () => {
     const component = shallow(
       <SocialIcon light raised={false} type="medium" />
+    );
+
+    expect(component.length).toBe(1);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('should have width and height based on iconSize', () => {
+    const component = shallow(
+      <SocialIcon button={false} light={false} raised={false} />
+    );
+
+    expect(component.length).toBe(1);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('should use style and font from props', () => {
+    const component = shallow(
+      <SocialIcon
+        button
+        light
+        title="title"
+        type="github"
+        fontFamily="arial"
+        fontWeight="bold"
+        style={{ backgroundColor: 'pink' }}
+        iconStyle={{ borderWidth: 1 }}
+        fontStyle={{ justifyContent: 'center' }}
+      />
     );
 
     expect(component.length).toBe(1);
@@ -45,6 +88,14 @@ describe('SocialIcon component', () => {
 
     component.simulate('press');
     expect(onPress).toHaveBeenCalled();
+  });
+
+  it('should NOT have onPress event when disabled', () => {
+    const onPress = jest.fn();
+    const component = shallow(<SocialIcon onPress={onPress} disabled />);
+
+    component.simulate('press');
+    expect(onPress).not.toHaveBeenCalled();
   });
 
   it('should apply values from theme', () => {
