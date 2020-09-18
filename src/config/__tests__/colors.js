e2335fedd4a2e7e606ed175a21de64ee37b80e19
Pranshu Chittora <pranshuchittora17@gmail.com>
2020-09-18 11:30:12 +0530

    feat(Colors): Dark colors are now available in the Public API

---
+++ src/config/__tests__/colors.js
@@ -0,0 +1,10 @@
+import { colors, colorsDark } from '..';
+
+describe('Color', () => {
+  it('Colors (Light) should exist', () => {
+    expect(colors).not.toBe(null);
+  });
+  it('Colors (Dark) should exist', () => {
+    expect(colorsDark).not.toBe(null);
+  });
+});
