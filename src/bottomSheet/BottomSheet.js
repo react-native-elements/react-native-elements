e6c2249511c56fb4a1ceed734f4ec66cba757483
Ralph de Ruijter <superdopey@live.nl>
2021-01-11 20:19:40 +0100

    added onBackdropPress

--- src/bottomSheet/BottomSheet.js
+++ src/bottomSheet/BottomSheet.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Modal, View, StyleSheet, ScrollView } from 'react-native';
+import { Modal, View, StyleSheet, ScrollView,Pressable } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { withTheme } from '../config';
 import PropTypes from 'prop-types';
@@ -9,6 +9,7 @@ function BottomSheet({
   isVisible,
   modalProps,
   children,
+  onBackdropPress,
   ...props
 }) {
   return (
@@ -25,32 +26,41 @@ function BottomSheet({
         ])}
         {...props}
       >
-        <View>
-          <ScrollView>{children}</ScrollView>
-        </View>
+         <Pressable
+        onPress={onBackdropPress}
+        style={styles.pressable}>
+          <View>
+            <ScrollView>{children}</ScrollView>
+          </View>
+        </Pressable>
       </SafeAreaView>
     </Modal>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({  
   safeAreaView: {
     flex: 1,
     backgroundColor: 'rgba(0,0,0,0.2)',
     flexDirection: 'column-reverse',
   },
   listContainer: { backgroundColor: 'white' },
+  pressable:{
+    flex: 1,
+  },
 });
 
 BottomSheet.defaultProps = {
   modalProps: {},
   isVisible: false,
+  onBackdropPress:()=>{}
 };
 
 BottomSheet.propTypes = {
   containerStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
   modalProps: PropTypes.object,
   isVisible: PropTypes.bool,
+  onBackdropPress: PropTypes.func
 };
 
 export { BottomSheet };
