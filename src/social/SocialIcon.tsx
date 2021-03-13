8425d6afe4292b8f54362d47ac5009372b9628b7
Ankush Dutt <ankushd100@gmail.com>
2021-03-13 18:26:57 +0530

    fix [socialicon]: icon visible in loading state when not a button

--- src/social/SocialIcon.tsx
+++ src/social/SocialIcon.tsx
@@ -134,6 +134,8 @@ const SocialIcon: React.FunctionComponent<SocialIconProps> = (props) => {
     ...attributes
   } = props;
 
+  const shouldShowExpandedButton = button && title;
+
   return (
     <Component
       {...attributes}
@@ -159,15 +161,18 @@ const SocialIcon: React.FunctionComponent<SocialIconProps> = (props) => {
       ])}
     >
       <View style={styles.wrapper}>
-        <Icon
+        {
+          (shouldShowExpandedButton || !loading) &&
+          <Icon
           //@ts-ignore
           iconStyle={StyleSheet.flatten([iconStyle && iconStyle])}
           color={light ? colors[type] : iconColor}
           name={type}
           size={iconSize}
           type={iconType}
-        />
-        {button && title && (
+          />
+        }
+        {shouldShowExpandedButton && (
           <Text
             //@ts-ignore
             style={StyleSheet.flatten([
@@ -189,7 +194,7 @@ const SocialIcon: React.FunctionComponent<SocialIconProps> = (props) => {
               styles.activityIndicatorStyle,
               activityIndicatorStyle,
             ])}
-            color={iconColor || 'white'}
+            color={light ? colors[type] : iconColor}
             size={(small && 'small') || 'large'}
           />
         )}
