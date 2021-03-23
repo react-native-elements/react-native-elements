ca287b816c6e991ad9d138c8ae755f2a535aaa67
Dhananjayan P N <pndjay@gmail.com>
2021-03-24 00:47:04 +0530

    adding actions

--- src/dialog/Dialog.tsx
+++ src/dialog/Dialog.tsx
@@ -1,29 +1,93 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, TextStyle, StyleProp } from 'react-native';
 import Overlay, { OverlayProps } from '../overlay/Overlay';
+import Button, { ButtonProps } from '../buttons/Button';
 import { withTheme } from '../config';
+import { TextProps } from '../text/Text';
 
 export type DialogProps = Omit<OverlayProps, 'fullScreen'> & {
   loading?: boolean;
+  title: string;
+  titleStyle?: StyleProp<TextStyle>;
+  titleProps?: TextProps;
+  body: string;
+  bodyStyle?: StyleProp<TextStyle>;
+  bodyProps?: TextProps;
+  primary?: string;
+  primaryOnPress?(): void;
+  secondary?: string;
+  secondaryOnPress?(): void;
+  buttonType?: 'solid' | 'clear' | 'outline';
+  buttonProps?: ButtonProps;
 };
 
 const Dialog: React.FunctionComponent<DialogProps> = ({
+  loading,
+  title,
+  titleStyle,
+  titleProps,
+  body,
+  bodyStyle,
+  bodyProps,
+  primary,
+  primaryOnPress,
+  secondary,
+  secondaryOnPress,
+  buttonType,
+  buttonProps,
   overlayStyle,
+  onBackdropPress,
   ...rest
 }: DialogProps) => {
   return (
     <Overlay
       {...rest}
+      onBackdropPress={onBackdropPress}
       overlayStyle={StyleSheet.flatten([styles.dialog, overlayStyle])}
     >
-      <Text>Hello! This is a dialog!</Text>
+      <Text
+        style={StyleSheet.flatten([styles.title, titleStyle])}
+        {...titleProps}
+      >
+        {title}
+      </Text>
+      <Text style={StyleSheet.flatten([styles.body, bodyStyle])} {...bodyProps}>
+        {body}
+      </Text>
+      <Button
+        type={buttonType}
+        onPress={primaryOnPress ?? onBackdropPress}
+        {...buttonProps}
+      >
+        {primary}
+      </Button>
+      {secondary !== null ? (
+        <Button type={buttonType} onPress={secondaryOnPress} {...buttonProps}>
+          {secondary}
+        </Button>
+      ) : null}
     </Overlay>
   );
+};
+
+Dialog.defaultProps = {
+  primary: 'Close',
+  secondary: null,
+  primaryOnPress: null,
+  secondaryOnPress: () => null,
+  buttonType: 'clear',
 };
 
 const styles = StyleSheet.create({
   dialog: {
     width: '75%',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  body: {
+    fontSize: 15,
   },
 });
 
