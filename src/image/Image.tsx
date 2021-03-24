25f5448b3da2aca22256d7c73395efcedfeab7ff
Hetan Thakkar <hetanthakkar1@gmail.com>
2021-03-25 02:26:03 +0530

    Removed ts-ignore from image component

--- src/image/Image.tsx
+++ src/image/Image.tsx
@@ -8,6 +8,7 @@ import {
   ImageProps as RNImageProps,
   ViewStyle,
   StyleProp,
+  ImageStyle,
 } from 'react-native';
 import { withTheme } from '../config';
 import { ThemeProps } from '../config';
@@ -88,15 +89,12 @@ class Image extends React.Component<
           transitionDuration={360}
           {...attributes}
           onLoad={this.onLoad}
-          // @ts-ignore
           style={StyleSheet.flatten([
             StyleSheet.absoluteFill,
             {
-              // @ts-ignore
               width: width,
               height: height,
-            },
-            // @ts-ignore
+            } as StyleProp<ImageStyle>,
             styleProps,
           ])}
         />
