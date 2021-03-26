5df0041b07793f9503bf54312d31077e15a9d61b
Khushal Agarwal <khushal.agarwal987@gmail.com>
2021-03-26 20:40:43 +0530

    added past changes

--- src/helpers/normalizeText.tsx
+++ src/helpers/normalizeText.tsx
@@ -1,13 +1,7 @@
-import { Dimensions } from 'react-native';
-
-let { width, height } = Dimensions.get('window');
+import { moderateScale } from 'react-native-size-matters';
 
 function normalize(number: number, factor = 0.25) {
-  const guidelineBaseWidth = 350;
-  const shortDimension = height < width ? height : width;
-  const scale = (size) => (shortDimension / guidelineBaseWidth) * size;
-  const moderateScale = number + (scale(number) - number) * factor;
-  return moderateScale;
+  return moderateScale(number, factor);
 }
 
 export default normalize;
