2bb4ace5b717e4a5d3f9dcd8bd9efffca2cc7abd
Hetan Thakkar <hetanthakkar1@gmail.com>
2021-03-29 00:35:57 +0530

    removed isTheme function

--- src/config/ThemeProvider.tsx
+++ src/config/ThemeProvider.tsx
@@ -54,12 +54,10 @@ export default class ThemeProvider extends React.Component<
 
   static getDerivedStateFromProps(props, state) {
     const { useDark } = props;
-    const isTheme = (theme) => {
-      return !(Object.keys(theme).length === 0 && theme.constructor === Object);
-    };
+
     if (
       useDark !== state.useDark ||
-      (isTheme(props.theme) && props.theme !== state.theme)
+      (props.theme !== {} && props.theme !== state.theme)
     ) {
       const defaultColors = useDark ? darkColors : colors;
       return {
