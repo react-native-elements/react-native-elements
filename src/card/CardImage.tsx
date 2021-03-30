70266f0512de8c4f1c20a1b977e39cf2bea5b16f
Abhishek Kumar <abhishek022kk@gmail.com>
2021-03-30 21:27:02 +0530

    add optional chaining

--- src/card/CardImage.tsx
+++ src/card/CardImage.tsx
@@ -10,7 +10,7 @@ const CardImage: RneFunctionComponent<ImageProps> = ({ style, ...props }) => {
 
 const styles = StyleSheet.create({
   image: {
-    width: undefined,
+    width: null,
     height: 150,
   },
 });
