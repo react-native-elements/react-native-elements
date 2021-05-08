7b5d571db139c6c60b8e08656a800a60edb468c2
HariAcidReign <haran465@gmail.com>
2021-05-08 23:08:21 +0530

    Test trial 1✅

---
+++ src/input/__tests__/FloatingTextInput.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import theme from '../../config/theme';
+import ThemedFloatingTextInput, {
+  FloatingTextInput,
+} from '../FloatingTextInput';
+
+describe('Input component', () => {
+  it('should match snapshot', () => {
+    const component = shallow(<FloatingTextInput theme={theme} />);
+    expect(component.length).toBe(1);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  describe('Props', () => {
+    it('attrName', () => {
+      const component = shallow(
+        <FloatingTextInput theme={theme} attrName="My Attribute Name" />
+      );
+      expect(component.length).toBe(1);
+      expect(toJson(component)).toMatchSnapshot();
+    });
+
+    it('title', () => {
+      const component = shallow(
+        <FloatingTextInput theme={theme} title="My Title Name" />
+      );
+      expect(component.length).toBe(1);
+      expect(toJson(component)).toMatchSnapshot();
+    });
+
+    it('value', () => {
+      const component = shallow(
+        <FloatingTextInput theme={theme} title="My Title Name" />
+      );
+      expect(component.length).toBe(1);
+      expect(toJson(component)).toMatchSnapshot();
+    });
+
+    it('titleActiveSize', () => {
+      const component = shallow(<FloatingTextInput titleActiveSize={14} />);
+      expect(component.length).toBe(1);
+      expect(toJson(component)).toMatchSnapshot();
+    });
+
+    it('titleInActiveSize', () => {
+      const component = shallow(<FloatingTextInput titleInActiveSize={19} />);
+      expect(component.length).toBe(1);
+      expect(toJson(component)).toMatchSnapshot();
+    });
+
+    it('titleActiveColor', () => {
+      const component = shallow(
+        <FloatingTextInput theme={theme} titleActiveColor="blue" />
+      );
+      expect(component.length).toBe(1);
+      expect(toJson(component)).toMatchSnapshot();
+    });
+
+    it('titleInActiveColor', () => {
+      const component = shallow(
+        <FloatingTextInput theme={theme} titleInActiveColor="dimgrey" />
+      );
+      expect(component.length).toBe(1);
+      expect(toJson(component)).toMatchSnapshot();
+    });
+  });
+
+  describe('Instance methods', () => {
+    it('should focus the input', () => {
+      const focus = jest.fn();
+      const component = shallow(<Input theme={theme} />);
+      const instance = component.instance();
+      // Refs not available in shallow render
+      instance.input = {
+        focus,
+      };
+      instance.handleFocus();
+      expect(focus).toHaveBeenCalledTimes(1);
+    });
+
+    it('should blur the input', () => {
+      const blur = jest.fn();
+      const component = shallow(<Input theme={theme} />);
+      const instance = component.instance();
+      // Refs not available in shallow render
+      instance.input = {
+        blur,
+      };
+      instance.handleBlur();
+      expect(blur).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  it('should apply values from theme', () => {
+    const testTheme = {
+      Input: {
+        placeholder: 'Enter text',
+      },
+    };
+    const component = create(
+      <ThemeProvider theme={testTheme}>
+        <ThemedFloatingTextInput />
+      </ThemeProvider>
+    );
+    expect(component.root.findByType(TextInput).props.placeholder).toBe(
+      'Enter text'
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
