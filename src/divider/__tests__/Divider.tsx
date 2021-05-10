d3009f9e611b47c5386b38b45131398d455bb58d
Khushal Agarwal <khushal.agarwal987@gmail.com>
2021-05-10 08:35:21 +0530

    Feat: added props to divider (#2988)
    * feat: added props to divider
    
    * feat: added orientation prop and test
    
    * feat: add subHeder, subHeaderStyle, width
    
    * docs: add divider docs
    
    * feat: improved subHeader prop

--- src/divider/__tests__/Divider.tsx
+++ src/divider/__tests__/Divider.tsx
@@ -15,11 +15,99 @@ describe('Divider Component', () => {
 
   it('should render with style', () => {
     const component = shallow(
-      <Divider theme={theme} style={{ backgroundColor: 'blue' }} />
+      <Divider theme={theme} style={{ borderLeftColor: 'blue' }} />
     );
     expect(component.length).toBe(1);
     expect(toJson(component)).toMatchSnapshot();
-    expect(component.props().style.backgroundColor).toBe('blue');
+  });
+
+  it('should render with color and orientation horizontal', () => {
+    const component = shallow(
+      <Divider theme={theme} color={theme.colors.primary} />
+    );
+    expect(component.length).toBe(1);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('should render with color and orientation vertical', () => {
+    const component = shallow(
+      <Divider
+        theme={theme}
+        color={theme.colors.primary}
+        orientation="vertical"
+      />
+    );
+    expect(component.length).toBe(1);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('should render with inset true and insetType left', () => {
+    const component = shallow(
+      <Divider theme={theme} inset={true} insetType="left" />
+    );
+    expect(component.length).toBe(1);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('should render with inset true and insetType right', () => {
+    const component = shallow(
+      <Divider theme={theme} inset={true} insetType="right" />
+    );
+    expect(component.length).toBe(1);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('should render with inset true and insetType middle', () => {
+    const component = shallow(
+      <Divider theme={theme} inset={true} insetType="middle" />
+    );
+    expect(component.length).toBe(1);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('should render with orientation vertical', () => {
+    const component = shallow(<Divider theme={theme} orientation="vertical" />);
+    expect(component.length).toBe(1);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('should render with width and orientation horizontal', () => {
+    const component = shallow(<Divider theme={theme} width={5} />);
+    expect(component.length).toBe(1);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('should render with width and orientation vertical', () => {
+    const component = shallow(
+      <Divider theme={theme} width={5} orientation="vertical" />
+    );
+    expect(component.length).toBe(1);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('should render with subHeader and subHeaderStyle', () => {
+    const component = shallow(
+      <Divider
+        theme={theme}
+        subHeader="Test"
+        subHeaderStyle={{ color: 'blue' }}
+      />
+    );
+    expect(component.length).toBe(1);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('should render with subHeader and subHeaderStyle and inset true', () => {
+    const component = shallow(
+      <Divider
+        theme={theme}
+        subHeader="Test"
+        inset={true}
+        subHeaderStyle={{ color: 'blue' }}
+      />
+    );
+    expect(component.length).toBe(1);
+    expect(toJson(component)).toMatchSnapshot();
   });
 
   it('should pass view properties', () => {
