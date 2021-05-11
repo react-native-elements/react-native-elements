e4c476b0e610085d1757242062ddf38ab7a8ec46
Dhananjayan P N <pndjay@gmail.com>
2021-05-12 03:39:42 +0530

    updated unit tests

--- src/dialog/__tests__/Dialog.js
+++ src/dialog/__tests__/Dialog.js
@@ -3,13 +3,36 @@ import { Text } from 'react-native';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import { create } from 'react-test-renderer';
-import theme from '../../config/theme';
 import { ThemeProvider } from '../../config';
 import ThemedDialog, { Dialog } from '../Dialog';
 
 describe('Dialog Component', () => {
-  it('should render without issues', () => {
-    const component = shallow(<Dialog theme={theme} />);
+  it('should render without any children', () => {
+    const component = shallow(<Dialog isVisible />);
+    expect(component.length).toBe(1);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('should render with title', () => {
+    const component = shallow(
+      <Dialog isVisible>
+        <Dialog.Title title="Dialog Title" />
+        <Text>This is a test dialog</Text>
+      </Dialog>
+    );
+    expect(component.length).toBe(1);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  it('should render without buttons', () => {
+    const component = shallow(
+      <Dialog isVisible>
+        <Text>This is a test dialog</Text>
+        <Dialog.Actions>
+          <Dialog.Button />
+        </Dialog.Actions>
+      </Dialog>
+    );
     expect(component.length).toBe(1);
     expect(toJson(component)).toMatchSnapshot();
   });
@@ -35,42 +58,6 @@ describe('Dialog Component', () => {
       component.find({ testID: 'Internal__Overlay' }).props().isVisible
     ).toBeFalsy();
   });
-
-  // it('should click the primary button and use passed handler', () => {
-  //   const primaryOnPress = jest.fn();
-  //   const wrapper = shallow(
-  //     <Dialog isVisible primaryOnPress={primaryOnPress}>
-  //       <Text>This is a test dialog</Text>
-  //     </Dialog>
-  //   );
-  //   wrapper
-  //     .find({ testID: 'Internal__Overlay' })
-  //     .dive()
-  //     .dive()
-  //     .find({ testID: 'Primary__Button' })
-  //     .simulate('press');
-  //   expect(primaryOnPress).toHaveBeenCalled();
-  // });
-
-  // it('should click the primary button and use passed handler', () => {
-  //   const secondaryOnPress = jest.fn();
-  //   const wrapper = shallow(
-  //     <Dialog
-  //       isVisible
-  //       secondary="Test Action"
-  //       secondaryOnPress={secondaryOnPress}
-  //     >
-  //       <Text>This is a test dialog</Text>
-  //     </Dialog>
-  //   );
-  //   wrapper
-  //     .find({ testID: 'Internal__Overlay' })
-  //     .dive()
-  //     .dive()
-  //     .find({ testID: 'Secondary__Button' })
-  //     .simulate('press');
-  //   expect(secondaryOnPress).toHaveBeenCalled();
-  // });
 
   it('should apply values from theme', () => {
     const testTheme = {
