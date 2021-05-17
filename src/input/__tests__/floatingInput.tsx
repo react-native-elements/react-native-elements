52cd5fd09cf83057ebb9dc80069e53f613f9d4e4
HariAcidReign <haran465@gmail.com>
2021-05-17 15:35:59 +0530

    Fixed test error

--- src/input/__tests__/floatingInput.tsx
+++ src/input/__tests__/floatingInput.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import theme from '../../config/theme';
-import { FloatingTextInput } from '../FloatingInput';
+import { FloatingTextInput } from '../floatingInput';
 import { Input } from '../Input';
 
 describe('Input component', () => {
