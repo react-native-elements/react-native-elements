03ac61be0d473fad602215458384885d54d03ece
Arpit Bhalla <arpitbhalla2001@gmail.com>
2021-05-20 18:28:09 +0530

    Updated

--- src/config/ThemeProvider.tsx
+++ src/config/ThemeProvider.tsx
@@ -24,19 +24,23 @@ export type ThemeProviderProps = {
   theme?: Partial<FullTheme>;
 };
 
-const ThemeProvider: React.FC<ThemeProviderProps> = (props) => {
-  const defaultColors = props.useDark ? darkColors : colors;
+const ThemeProvider: React.FC<ThemeProviderProps> = ({
+  theme = {},
+  children,
+  useDark,
+}) => {
+  const defaultColors = useDark ? darkColors : colors;
   const [themeState, setThemeState] = React.useState<Partial<FullTheme>>(
     deepmerge(
       {
         colors: defaultColors,
       },
-      props.theme
+      theme
     )
   );
 
   React.useEffect(() => {
-    if (isEqual(themeState, props.theme)) {
+    if (isEqual(themeState, theme)) {
       setThemeState((prevState) =>
         deepmerge(
           prevState,
@@ -44,12 +48,12 @@ const ThemeProvider: React.FC<ThemeProviderProps> = (props) => {
             {
               colors: defaultColors,
             },
-            props.theme
+            theme
           )
         )
       );
     }
-  }, [props.theme, defaultColors, themeState]);
+  }, [theme, defaultColors, themeState]);
 
   const updateTheme = (newTheme: RecursivePartial<FullTheme>) => {
     setThemeState(deepmerge(themeState, newTheme));
@@ -62,7 +66,7 @@ const ThemeProvider: React.FC<ThemeProviderProps> = (props) => {
           {
             colors: defaultColors,
           },
-          props.theme
+          theme
         ),
         newTheme
       )
@@ -77,7 +81,7 @@ const ThemeProvider: React.FC<ThemeProviderProps> = (props) => {
         replaceTheme: replaceTheme,
       }}
     >
-      {props.children}
+      {children}
     </ThemeContext.Provider>
   );
 };
