4ec93fd5f2b346aeea91d13fd9dcdf0224875133
Arpit Bhalla <arpitbhalla2001@gmail.com>
2021-06-22 14:51:01 +0530

    FC to FWDREF

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -13,10 +13,11 @@ import {
   Pressable,
 } from 'react-native';
 import Color from 'color';
-import { renderNode, color, RneFunctionComponent } from '../helpers';
+import { renderNode, color } from '../helpers';
 import Icon, { IconNode } from '../Icon';
 import { Theme } from '../config/theme';
 import { TextProps } from '../Text';
+import { ThemeProps } from '../config';
 
 const defaultLoadingProps = (
   type: 'solid' | 'clear' | 'outline',
@@ -49,161 +50,170 @@ export type ButtonProps = PressableProps & {
   iconPosition?: 'left' | 'right' | 'top' | 'bottom';
 };
 
-export const Button: RneFunctionComponent<ButtonProps> = ({
-  Component = Pressable,
-  containerStyle,
-  onPress = () => console.log('Please attach a method to this component'),
-  buttonStyle,
-  type = 'solid',
-  loading = false,
-  loadingStyle,
-  loadingProps: passedLoadingProps,
-  title = '',
-  titleProps,
-  titleStyle: passedTitleStyle,
-  icon,
-  iconContainerStyle,
-  iconRight = false,
-  disabled = false,
-  disabledStyle,
-  disabledTitleStyle,
-  raised = false,
-  linearGradientProps,
-  ViewComponent = View,
-  theme,
-  iconPosition = 'left',
-  ...attributes
-}) => {
-  useEffect(() => {
-    if (linearGradientProps && !ViewComponent) {
-      console.error(
-        "You need to pass a ViewComponent to use linearGradientProps !\nExample: ViewComponent={require('react-native-linear-gradient')}"
-      );
-    }
-  });
-
-  const handleOnPress = useCallback(
-    (evt) => {
-      if (!loading) {
-        onPress(evt);
-      }
-    },
-    [loading, onPress]
-  );
-
-  const titleStyle: StyleProp<TextStyle> = StyleSheet.flatten([
+export const Button = React.forwardRef<
+  View,
+  ButtonProps & Partial<ThemeProps<ButtonProps>>
+>(
+  (
     {
-      color: type === 'solid' ? 'white' : theme?.colors?.primary,
+      Component = Pressable,
+      containerStyle,
+      onPress = () => console.log('Please attach a method to this component'),
+      buttonStyle,
+      type = 'solid',
+      loading = false,
+      loadingStyle,
+      loadingProps: passedLoadingProps,
+      title = '',
+      titleProps,
+      titleStyle: passedTitleStyle,
+      icon,
+      iconContainerStyle,
+      iconRight = false,
+      disabled = false,
+      disabledStyle,
+      disabledTitleStyle,
+      raised = false,
+      linearGradientProps,
+      ViewComponent = View,
+      theme,
+      iconPosition = 'left',
+      ...attributes
     },
-    styles.title,
-    passedTitleStyle,
-    disabled && {
-      color: color(theme?.colors?.disabled).darken(0.3).string(),
-    },
-    disabled && disabledTitleStyle,
-  ]);
+    ref: React.Ref<View>
+  ) => {
+    useEffect(() => {
+      if (linearGradientProps && !ViewComponent) {
+        console.error(
+          "You need to pass a ViewComponent to use linearGradientProps !\nExample: ViewComponent={require('react-native-linear-gradient')}"
+        );
+      }
+    });
 
-  const loadingProps: ActivityIndicatorProps = {
-    ...defaultLoadingProps(type, theme),
-    ...passedLoadingProps,
-  };
+    const handleOnPress = useCallback(
+      (evt) => {
+        if (!loading) {
+          onPress(evt);
+        }
+      },
+      [loading, onPress]
+    );
 
-  const accessibilityState = {
-    disabled: !!disabled,
-    busy: !!loading,
-  };
-  const positionStyle = {
-    top: 'column',
-    bottom: 'column-reverse',
-    left: 'row',
-    right: 'row-reverse',
-  };
+    const titleStyle: StyleProp<TextStyle> = StyleSheet.flatten([
+      {
+        color: type === 'solid' ? 'white' : theme?.colors?.primary,
+      },
+      styles.title,
+      passedTitleStyle,
+      disabled && {
+        color: color(theme?.colors?.disabled).darken(0.3).string(),
+      },
+      disabled && disabledTitleStyle,
+    ]);
 
-  return (
-    <View
-      style={[
-        styles.container,
-        {
-          borderRadius: 3 || styles.container.borderRadius,
-        },
-        containerStyle,
-        raised && !disabled && type !== 'clear' && styles.raised,
-      ]}
-    >
-      <Component
-        onPress={handleOnPress}
-        delayPressIn={0}
-        activeOpacity={0.3}
-        accessibilityRole="button"
-        accessibilityState={accessibilityState}
-        disabled={disabled}
-        android_ripple={{
-          color: Color(titleStyle?.color?.toString())
-            .alpha(0.32)
-            .rgb()
-            .string(),
-          borderless: false,
-          radius: -5,
-        }}
-        {...attributes}
+    const loadingProps: ActivityIndicatorProps = {
+      ...defaultLoadingProps(type, theme),
+      ...passedLoadingProps,
+    };
+
+    const accessibilityState = {
+      disabled: !!disabled,
+      busy: !!loading,
+    };
+    const positionStyle = {
+      top: 'column',
+      bottom: 'column-reverse',
+      left: 'row',
+      right: 'row-reverse',
+    };
+
+    return (
+      <View
+        style={[
+          styles.container,
+          {
+            borderRadius: 3 || styles.container.borderRadius,
+          },
+          containerStyle,
+          raised && !disabled && type !== 'clear' && styles.raised,
+        ]}
       >
-        <ViewComponent
-          {...linearGradientProps}
-          style={StyleSheet.flatten([
-            styles.button,
-            styles.buttonOrientation,
-            {
-              flexDirection:
-                positionStyle[iconRight ? 'right' : iconPosition] || 'row',
-            },
-            {
-              backgroundColor:
-                type === 'solid' ? theme?.colors?.primary : 'transparent',
-              borderColor: theme?.colors?.primary,
-              borderWidth: type === 'outline' ? StyleSheet.hairlineWidth : 0,
-            },
-            buttonStyle,
-            disabled &&
-              type === 'solid' && {
-                backgroundColor: theme?.colors?.disabled,
-              },
-            disabled &&
-              type === 'outline' && {
-                borderColor: color(theme?.colors?.disabled)
-                  .darken(0.3)
-                  .string(),
-              },
-            disabled && disabledStyle,
-          ])}
+        <Component
+          ref={ref}
+          onPress={handleOnPress}
+          delayPressIn={0}
+          activeOpacity={0.3}
+          accessibilityRole="button"
+          accessibilityState={accessibilityState}
+          disabled={disabled}
+          android_ripple={{
+            color: Color(titleStyle?.color?.toString())
+              .alpha(0.32)
+              .rgb()
+              .string(),
+            borderless: false,
+            radius: -5,
+          }}
+          {...attributes}
         >
-          {loading && (
-            <ActivityIndicator
-              style={StyleSheet.flatten([styles.loading, loadingStyle])}
-              color={loadingProps.color}
-              size={loadingProps.size}
-              {...loadingProps}
-            />
-          )}
-          {!loading &&
-            icon &&
-            renderNode(Icon, icon, {
-              containerStyle: StyleSheet.flatten([
-                styles.iconContainer,
-                iconContainerStyle,
-              ]),
-            })}
+          <ViewComponent
+            {...linearGradientProps}
+            style={StyleSheet.flatten([
+              styles.button,
+              styles.buttonOrientation,
+              {
+                flexDirection:
+                  positionStyle[iconRight ? 'right' : iconPosition] || 'row',
+              },
+              {
+                backgroundColor:
+                  type === 'solid' ? theme?.colors?.primary : 'transparent',
+                borderColor: theme?.colors?.primary,
+                borderWidth: type === 'outline' ? StyleSheet.hairlineWidth : 0,
+              },
+              buttonStyle,
+              disabled &&
+                type === 'solid' && {
+                  backgroundColor: theme?.colors?.disabled,
+                },
+              disabled &&
+                type === 'outline' && {
+                  borderColor: color(theme?.colors?.disabled)
+                    .darken(0.3)
+                    .string(),
+                },
+              disabled && disabledStyle,
+            ])}
+          >
+            {loading && (
+              <ActivityIndicator
+                style={StyleSheet.flatten([styles.loading, loadingStyle])}
+                color={loadingProps.color}
+                size={loadingProps.size}
+                {...loadingProps}
+              />
+            )}
+            {!loading &&
+              icon &&
+              renderNode(Icon, icon, {
+                containerStyle: StyleSheet.flatten([
+                  styles.iconContainer,
+                  iconContainerStyle,
+                ]),
+              })}
 
-          {!loading &&
-            !!title &&
-            renderNode(Text, title, {
-              style: titleStyle,
-              ...titleProps,
-            })}
-        </ViewComponent>
-      </Component>
-    </View>
-  );
-};
+            {!loading &&
+              !!title &&
+              renderNode(Text, title, {
+                style: titleStyle,
+                ...titleProps,
+              })}
+          </ViewComponent>
+        </Component>
+      </View>
+    );
+  }
+);
 
 const styles = StyleSheet.create({
   button: {
