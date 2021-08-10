ad70022c395184fe27b28fbea1a48cb374223d78
Khushal Agarwal <khushal.agarwal987@gmail.com>
2021-08-10 10:21:37 +0530

    [update] khushal87 pre-push script

--- updateDocumentation.js
+++ updateDocumentation.js
@@ -1,22 +1,19 @@
 const { exec } = require('child_process');
 
-exec('yarn docs:build', (err, stdout, stderr) => {
+exec('yarn docs:build', (err) => {
   if (err) {
     return;
   }
-  exec('git status --porcelain', (err, stdout, stderr) => {
+  exec('git status --porcelain', (err, stdout) => {
     if (err) {
       return;
     }
     if (stdout.length) {
-      exec(
-        "git add '*.md' && git commit -m 'Update Documentation'",
-        (err, stdout, stderr) => {
-          if (err) {
-            return;
-          }
+      exec("git add '*.md' && git commit -m 'Update Documentation'", (err) => {
+        if (err) {
+          return;
         }
-      );
+      });
     } else {
       console.log('No changes to commit.');
     }
