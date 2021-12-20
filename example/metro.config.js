32f6b9b6fbdca10e66fcbfea6db06350bc3de6dc
Arpit Bhalla <arpitbhalla2001@gmail.com>
2021-12-20 23:03:28 +0530

    added

--- example/metro.config.js
+++ example/metro.config.js
@@ -3,13 +3,14 @@ const { getDefaultConfig } = require('expo/metro-config');
 const path = require('path');
 
 // Find the workspace root, this can be replaced with `find-yarn-workspace-root`
-const workspaceRoot = path.resolve(__dirname, '../..');
+const workspaceRoot = path.resolve(__dirname, '..');
 const projectRoot = __dirname;
 
 const config = getDefaultConfig(projectRoot);
 
 // 1. Watch all files within the monorepo
-config.watchFolders = [workspaceRoot];
+// config.watchFolders = [path.resolve(__dirname, '../packages')];
+
 // 2. Let Metro know where to resolve packages, and in what order
 config.resolver.nodeModulesPath = [
   path.resolve(projectRoot, 'node_modules'),
