bc457220d18555ec1830a526c5c0d1cc3b5c20c4
Arpit Bhalla <arpitbhalla2001@gmail.com>
2021-12-29 16:00:54 +0530

    :sparkles: Improved

--- example/index.tsx
+++ example/index.tsx
@@ -1,6 +1,28 @@
+import React from 'react';
 import { registerRootComponent } from 'expo';
+import { Text, View, StyleSheet } from 'react-native';
+import { ButtonComp } from '@react-native-elements/theme';
+// import { Tab } from '@react-native-elements/base';
 
-import App from './App';
+// import App from './App';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+const App = () => (
+  <View style={styles.container}>
+    {/* <Tab value={2}>
+      <Tab.Item title="Tab 1" />
+      <Tab.Item title="Tab 2" />
+    </Tab> */}
+    <Text>Work</Text>
+    <ButtonComp />
+  </View>
+);
 
 // // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // // It also ensures that whether you load the app in Expo Go or in a native build,
--- example/metro.config.js
+++ example/metro.config.js
@@ -1,77 +1,23 @@
 const path = require('path');
-const fs = require('fs');
-const escape = require('escape-string-regexp');
 const blacklist = require('metro-config/src/defaults/blacklist');
 
-const root = path.resolve(__dirname, '..');
-const packages = path.resolve(root, 'packages');
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
 
-// List all packages under `packages/`
-const workspaces = fs
-  .readdirSync(packages)
-  .map((p) => path.join(packages, p))
-  .filter(
-    (p) =>
-      fs.statSync(p).isDirectory() &&
-      fs.existsSync(path.join(p, 'package.json'))
-  );
+// Find the workspace root, this can be replaced with `find-yarn-workspace-root`
+const workspaceRoot = path.resolve(__dirname, '../..');
+const projectRoot = __dirname;
 
-// Get the list of dependencies for all packages in the monorepo
-const modules = ['@expo/vector-icons']
-  .concat(
-    ...workspaces.map((it) => {
-      const pak = JSON.parse(
-        fs.readFileSync(path.join(it, 'package.json'), 'utf8')
-      );
+const config = getDefaultConfig(projectRoot);
 
-      // We need to make sure that only one version is loaded for peerDependencies
-      // So we blacklist them at the root, and alias them to the versions in example's node_modules
-      return pak.peerDependencies ? Object.keys(pak.peerDependencies) : [];
-    })
-  )
-  .sort()
-  .filter(
-    (m, i, self) =>
-      // Remove duplicates and package names of the packages in the monorepo
-      self.lastIndexOf(m) === i && !m.startsWith('@react-native-elements/')
-  );
+// 1. Watch all files within the monorepo
+config.watchFolders = [workspaceRoot];
+// 2. Let Metro know where to resolve packages, and in what order
+config.resolver.nodeModulesPath = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
 
-module.exports = {
-  projectRoot: __dirname,
+config.resolver.blacklistRE = blacklist([/website\/.*/]);
 
-  // We need to watch the root of the monorepo
-  // This lets Metro find the monorepo packages automatically using haste
-  // This also lets us import modules from monorepo root
-  watchFolders: [root],
-
-  resolver: {
-    // We need to blacklist the peerDependencies we've collected in packages' node_modules
-    blacklistRE: blacklist(
-      [/website\/.*/].concat(
-        ...workspaces.map((it) =>
-          modules.map(
-            (m) =>
-              new RegExp(`^${escape(path.join(it, 'node_modules', m))}\\/.*$`)
-          )
-        )
-      )
-    ),
-
-    // When we import a package from the monorepo, metro won't be able to find their deps
-    // We need to specify them in `extraNodeModules` to tell metro where to find them
-    extraNodeModules: modules.reduce((acc, name) => {
-      acc[name] = path.join(root, 'node_modules', name);
-      return acc;
-    }, {}),
-  },
-
-  transformer: {
-    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
-};
+module.exports = config;
