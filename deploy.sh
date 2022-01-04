6c3cd2099cbd6b6b234cef493f58a947e37fe1f6
Arpit Bhalla <arpitbhalla2001@gmail.com>
2022-01-04 21:48:51 +0530

    :sparkles: Improved

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-for pkg in $(find ./packages -mindepth 1 -maxdepth 1 -type d -printf '%f\n'); do
-    echo "@react-native-elements/$pkg"
-    git push a `git subtree split --prefix packages/$pkg -b $pkg`:refs/heads/$pkg --force
-done
