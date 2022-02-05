5a173d1d9ecaaae84f6d434c0ec93736ce5db11c
Arpit Bhalla <arpitbhalla2001@gmail.com>
2022-02-05 19:33:17 +0530

    example: fix module resolver

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,7 +1,23 @@
+var path = require('path');
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@react-native-elements/base/dist': path.resolve(
+              __dirname,
+              '..',
+              'packages/base/src'
+            ),
+          },
+        },
+      ],
+    ],
   };
 };
