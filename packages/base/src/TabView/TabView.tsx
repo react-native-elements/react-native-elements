69b43caa95a4a4dc08a9cb46a6f942f1a869bb81
Arpit Bhalla <55053424+arpitBhalla@users.noreply.github.com>
2022-03-18 22:55:51 +0530

    update

--- packages/base/src/TabView/TabView.tsx
+++ packages/base/src/TabView/TabView.tsx
@@ -77,9 +77,8 @@ export const TabViewBase: RneFunctionComponent<TabViewProps> = ({
   minSwipeSpeed = 1,
 }) => {
   const translateX = React.useRef(new Animated.Value(0));
-  const panX = React.useRef(value);
+  const panX = React.useRef(0);
   const [containerWidth, setContainerWidth] = React.useState(1);
-  const containerWidthRef = React.useRef(1);
 
   const childCount = React.useMemo(
     () => React.Children.count(children),
@@ -100,39 +99,41 @@ export const TabViewBase: RneFunctionComponent<TabViewProps> = ({
 
   const releaseResponder = React.useCallback(
     (_: GestureResponderEvent, { dx, vx }: PanResponderGestureState) => {
-      const position = dx / -containerWidthRef.current;
+      const position = dx / -containerWidth;
       const shouldSwipe =
         Math.abs(position) > minSwipeRatio || Math.abs(vx) > minSwipeSpeed;
       panX.current += shouldSwipe ? Math.sign(position) : 0;
       animate(panX.current);
       onChange(panX.current);
     },
-    [animate, minSwipeRatio, minSwipeSpeed, onChange]
+    [animate, containerWidth, minSwipeRatio, minSwipeSpeed, onChange]
   );
 
-  const { current: panResponder } = React.useRef(
-    PanResponder.create({
-      onPanResponderGrant: (_, { vx }) => {
-        onSwipeStart(vx > 0 ? 'left' : 'right');
-      },
-      onMoveShouldSetPanResponder: (_, { dx, dy, vx, vy }) => {
-        const panXInt = Math.floor(panX.current);
-        return (
-          !(
-            (dx > 0 && panXInt <= 0) ||
-            (dx < 0 && panXInt >= childCount - 1)
-          ) &&
-          Math.abs(dx) > Math.abs(dy) * 2 &&
-          Math.abs(vx) > Math.abs(vy) * 2.5
-        );
-      },
-      onPanResponderMove: (_, { dx }) => {
-        const position = dx / -containerWidthRef.current;
-        translateX.current.setValue(Math.floor(panX.current) + position);
-      },
-      onPanResponderRelease: releaseResponder,
-      onPanResponderTerminate: releaseResponder,
-    })
+  const panResponder = React.useMemo(
+    () =>
+      PanResponder.create({
+        onPanResponderGrant: (_, { vx }) => {
+          onSwipeStart(vx > 0 ? 'left' : 'right');
+        },
+        onMoveShouldSetPanResponder: (_, { dx, dy, vx, vy }) => {
+          const panXInt = Math.floor(panX.current);
+          return (
+            !(
+              (dx > 0 && panXInt <= 0) ||
+              (dx < 0 && panXInt >= childCount - 1)
+            ) &&
+            Math.abs(dx) > Math.abs(dy) * 2 &&
+            Math.abs(vx) > Math.abs(vy) * 2.5
+          );
+        },
+        onPanResponderMove: (_, { dx }) => {
+          const position = dx / -containerWidth;
+          translateX.current.setValue(Math.floor(panX.current) + position);
+        },
+        onPanResponderRelease: releaseResponder,
+        onPanResponderTerminate: releaseResponder,
+      }),
+    [childCount, containerWidth, onSwipeStart, releaseResponder]
   );
 
   React.useEffect(() => {
@@ -147,7 +148,6 @@ export const TabViewBase: RneFunctionComponent<TabViewProps> = ({
       style={[styles.container, containerStyle]}
       onLayout={({ nativeEvent: { layout } }) => {
         setContainerWidth(layout.width);
-        containerWidthRef.current = layout.width;
       }}
     >
       <Animated.View
