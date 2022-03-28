391dc8221a20a791de6dd3594c7baae21928f194
Arpit Bhalla <55053424+arpitBhalla@users.noreply.github.com>
2022-03-29 00:49:55 +0530

    website: upgrade to docusaurus@2.0.0-beta.18 (#3422)

---
+++ website/src/theme/ColorModeToggle/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ColorModeToggle from '@theme-original/ColorModeToggle';
+
+export default function ColorModeToggleWrapper(props) {
+  return (
+    <>
+      <ColorModeToggle {...props} />
+    </>
+  );
+}
