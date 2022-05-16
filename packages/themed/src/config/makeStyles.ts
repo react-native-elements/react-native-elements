7f657d3fecb008b526e7af3744a552e46d4c8439
Arpit Bhalla <55053424+arpitBhalla@users.noreply.github.com>
2022-05-16 23:21:09 +0530

    update

--- packages/themed/src/config/makeStyles.ts
+++ packages/themed/src/config/makeStyles.ts
@@ -1,7 +1,7 @@
 import { useMemo } from 'react';
 import { StyleSheet } from 'react-native';
 import { Colors } from './colors';
-import { ThemeMode } from './theme';
+import { ThemeMode, Theme } from './theme';
 import { useTheme } from './ThemeProvider';
 
 export const makeStyles =
@@ -12,7 +12,7 @@ export const makeStyles =
           theme: {
             colors: Colors;
             mode: ThemeMode;
-          },
+          } & Theme,
           props: V
         ) => T)
   ) =>
