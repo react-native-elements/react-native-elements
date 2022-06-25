c446031692cf6e27356ff40525a369af7c4cdbd2
Arpit Bhalla <55053424+arpitBhalla@users.noreply.github.com>
2022-06-25 16:37:32 +0530

    add tests

--- packages/themed/src/config/__tests__/ThemeProvider.test.tsx
+++ packages/themed/src/config/__tests__/ThemeProvider.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import Button from '../../Button';
+import Button, { Button as BaseButton } from '../../Button';
 import Text from '../../Text';
-import { Button as BaseButton } from '@rneui/base';
 import { lightColors } from '..';
 import { renderWithWrapper } from '../../../.ci/testHelper';
 import { fireEvent, render } from '@testing-library/react-native';
@@ -29,126 +28,125 @@ describe('ThemeProvider', () => {
         </ThemeProvider>
       </>
     );
-    // const buttons = queryAllByRole('button');
-    // buttons.forEach((el) => {
-    //   const size = el.props.testID;
-    //   console.log(size);
-    //   expect((el.children[0] as ReactTestInstance).props?.style).toMatchObject({
-    //     borderRadius: defaultSpacing[size],
-    //   });
-    // });
+    const buttons = queryAllByRole('button');
+    buttons.forEach((el) => {
+      const size = el.props.testID;
+      expect((el.children[0] as ReactTestInstance).props?.style).toMatchObject({
+        borderRadius: defaultSpacing[size],
+      });
+    });
     expect(toJSON()).toMatchSnapshot();
   });
 
-  // it('should update and replace theme', () => {
-  //   const TestComp = (): JSX.Element => {
-  //     const { theme, replaceTheme, updateTheme } = useTheme();
-  //     return (
-  //       <>
-  //         <Button
-  //           testID="updateTheme"
-  //           onPress={() => {
-  //             updateTheme({
-  //               lightColors: {
-  //                 primary: 'red',
-  //               },
-  //             });
-  //           }}
-  //         />
-  //         <Button
-  //           testID="replaceThemeButton"
-  //           onPress={() => {
-  //             replaceTheme({
-  //               lightColors: {
-  //                 primary: 'purple',
-  //               },
-  //             });
-  //           }}
-  //         />
-  //         <Text testID="themeChild" children={theme.colors.primary} />
-  //       </>
-  //     );
-  //   };
+  it('should update and replace theme', () => {
+    const TestComp = (): JSX.Element => {
+      const { theme, replaceTheme, updateTheme } = useTheme();
+      return (
+        <>
+          <Button
+            testID="updateTheme"
+            onPress={() => {
+              updateTheme({
+                lightColors: {
+                  primary: 'red',
+                },
+              });
+            }}
+          />
+          <Button
+            testID="replaceThemeButton"
+            onPress={() => {
+              replaceTheme({
+                lightColors: {
+                  primary: 'purple',
+                },
+              });
+            }}
+          />
+          <Text testID="themeChild" children={theme.colors.primary} />
+        </>
+      );
+    };
 
-  //   const { queryByTestId } = render(
-  //     <ThemeProvider theme={createTheme({})}>
-  //       <TestComp />
-  //     </ThemeProvider>
-  //   );
-  //   const updateButton = queryByTestId('updateTheme')!;
-  //   const replaceButton = queryByTestId('replaceThemeButton')!;
-  //   const textTheme = queryByTestId('themeChild')!;
-  //   expect(textTheme.props.children).toEqual(lightColors.primary);
+    const { queryByTestId } = render(
+      <ThemeProvider theme={createTheme({})}>
+        <TestComp />
+      </ThemeProvider>
+    );
+    const updateButton = queryByTestId('updateTheme')!;
+    const replaceButton = queryByTestId('replaceThemeButton')!;
+    const textTheme = queryByTestId('themeChild')!;
+    expect(textTheme.props.children).toEqual(lightColors.primary);
 
-  //   fireEvent.press(updateButton);
-  //   expect(textTheme.props.children).toEqual('red');
+    fireEvent.press(updateButton);
+    expect(textTheme.props.children).toEqual('red');
 
-  //   fireEvent.press(replaceButton);
-  //   expect(textTheme.props.children).toBe('purple');
-  // });
+    fireEvent.press(replaceButton);
+    expect(textTheme.props.children).toBe('purple');
+  });
 
-  // it('should use default theme', () => {
-  //   const { queryByTestId } = renderWithWrapper(
-  //     <ThemeProvider>
-  //       <ThemeConsumer>
-  //         {({ theme }) => (
-  //           <View testID="viewComp">{JSON.stringify(theme)}</View>
-  //         )}
-  //       </ThemeConsumer>
-  //     </ThemeProvider>
-  //   );
-  //   const instance = queryByTestId('viewComp')!;
-  //   expect(JSON.parse(instance.props.children)).toMatchObject({
-  //     colors: lightColors,
-  //   });
-  // });
+  it('should use default theme', () => {
+    const { queryByTestId } = renderWithWrapper(
+      <ThemeProvider>
+        <ThemeConsumer>
+          {({ theme }) => (
+            <View testID="viewComp">{JSON.stringify(theme)}</View>
+          )}
+        </ThemeConsumer>
+      </ThemeProvider>
+    );
+    const instance = queryByTestId('viewComp')!;
+    expect(JSON.parse(instance.props.children)).toMatchObject({
+      colors: lightColors,
+    });
+  });
 
-  // it('should retain custom theme when switching between light / dark mode', () => {
-  //   const customTheme = createTheme({
-  //     lightColors: {
-  //       primary: 'white',
-  //     },
-  //     darkColors: {
-  //       primary: 'black',
-  //     },
-  //     mode: 'light',
-  //     Text: { accessibilityLabel: 'theme-test' },
-  //   });
-  //   const TestComp = (): JSX.Element => {
-  //     const { theme, updateTheme } = useTheme();
+  it('should retain custom theme when switching between light / dark mode', () => {
+    const customTheme = createTheme({
+      lightColors: {
+        primary: 'white',
+      },
+      darkColors: {
+        primary: 'black',
+      },
+      mode: 'light',
+      Text: { accessibilityLabel: 'theme-test' },
+    });
+    const TestComp = (): JSX.Element => {
+      const { theme, updateTheme } = useTheme();
 
-  //     return (
-  //       <>
-  //         <Button
-  //           testID="updateTheme"
-  //           onPress={() =>
-  //             updateTheme((myTheme) => ({
-  //               mode: myTheme.mode === 'dark' ? 'light' : 'dark',
-  //             }))
-  //           }
-  //         />
-  //         <Text testID="themeChild" children={String(theme.mode)} />
-  //       </>
-  //     );
-  //   };
-  //   const { queryByTestId } = render(
-  //     <ThemeProvider theme={customTheme}>
-  //       <TestComp />
-  //     </ThemeProvider>
-  //   );
-  //   const updateButton = queryByTestId('updateTheme')!;
-  //   const textTheme = queryByTestId('themeChild')!;
+      return (
+        <>
+          <Button
+            testID="updateTheme"
+            onPress={() =>
+              updateTheme((myTheme) => ({
+                mode: myTheme.mode === 'dark' ? 'light' : 'dark',
+              }))
+            }
+          />
+          <Text testID="themeChild" children={String(theme.mode)} />
+        </>
+      );
+    };
+    const { queryByTestId } = render(
+      <ThemeProvider theme={customTheme}>
+        <TestComp />
+      </ThemeProvider>
+    );
+    const updateButton = queryByTestId('updateTheme')!;
+    const textTheme = queryByTestId('themeChild')!;
 
-  //   expect(textTheme.props.accessibilityLabel).toEqual('theme-test');
+    expect(textTheme.props.accessibilityLabel).toEqual('theme-test');
 
-  //   // set to dark mode
-  //   fireEvent.press(updateButton);
-  //   expect(textTheme.props.accessibilityLabel).toEqual('theme-test');
-  //   expect(textTheme.props.children).toEqual('dark');
+    // set to dark mode
+    fireEvent.press(updateButton);
+    expect(textTheme.props.accessibilityLabel).toEqual('theme-test');
+    expect(textTheme.props.children).toEqual('dark');
 
-  //   // set to light mode
-  //   fireEvent.press(updateButton);
-  //   expect(textTheme.props.accessibilityLabel).toEqual('theme-test');
-  //   expect(textTheme.props.children).toEqual('light');
-  // });
+    // set to light mode
+    fireEvent.press(updateButton);
+    expect(textTheme.props.accessibilityLabel).toEqual('theme-test');
+    expect(textTheme.props.children).toEqual('light');
+  });
 });
