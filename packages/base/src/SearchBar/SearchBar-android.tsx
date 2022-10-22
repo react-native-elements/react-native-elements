7960f4739e094b76ae342f3bb23aab6443b709a9
Arpit Bhalla <55053424+arpitBhalla@users.noreply.github.com>
2022-10-22 14:06:05 +0530

    feat(SeachBar): add `onKeyboardHide` for android (#3657)

--- packages/base/src/SearchBar/SearchBar-android.tsx
+++ packages/base/src/SearchBar/SearchBar-android.tsx
@@ -103,13 +103,16 @@ export class SearchBarAndroid extends Component<
       hasFocus: false,
       isEmpty: value ? value === '' : true,
     };
-    this.keyboardListener = Keyboard.addListener(
-      'keyboardDidHide',
-      this._keyboardDidHide
-    );
+    if (this.props.onKeyboardHide) {
+      this.keyboardListener = Keyboard.addListener(
+        'keyboardDidHide',
+        this._keyboardDidHide
+      );
+    }
   }
   _keyboardDidHide = () => {
-    this.cancel();
+    this.blur();
+    this.props.onKeyboardHide?.();
   };
 
   componentWillUnmount() {
