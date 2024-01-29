cec04af50a2cf163e721ad6e74d052ce94aa45c7
Niccolò Belli <niccolo.belli@linuxsystems.it>
2024-01-29 06:19:55 +0100

    fix(SocialIcon): handle button prop correctly (#3860)

--- packages/base/src/SocialIcon/SocialIcon.tsx
+++ packages/base/src/SocialIcon/SocialIcon.tsx
@@ -212,9 +212,9 @@ export const SocialIcon: RneFunctionComponent<SocialIconProps> = ({
             borderRadius: iconSize * 2,
           },
         { backgroundColor: type && colors[type] },
-        {
+        { height: iconSize * 2 + 4 },
+        !button && {
           width: iconSize * 2 + 4,
-          height: iconSize * 2 + 4,
           borderRadius: iconSize * 2,
         },
         light && { backgroundColor: 'white' },
@@ -274,8 +274,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   button: {
-    paddingTop: 14,
-    paddingBottom: 14,
+    paddingVertical: 14,
+    paddingHorizontal: 21,
   },
   raised: {
     ...Platform.select({
